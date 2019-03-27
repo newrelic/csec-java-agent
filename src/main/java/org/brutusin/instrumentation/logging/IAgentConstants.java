5a358806a854d87622b2774386dc44031fe4f85b
lovesh-ap <lovesh@k2io.com>
2019-03-27 17:51:46 +0530

    update get connection map

--- src/main/java/org/brutusin/instrumentation/logging/IAgentConstants.java
+++ src/main/java/org/brutusin/instrumentation/logging/IAgentConstants.java
@@ -16,7 +16,7 @@ public interface IAgentConstants {
 
 	Map<String, List<String>> MYSQL_GET_CONNECTION_MAP = new HashMap() {
 		{
-			put("java.sql.DriverManager", Collections.singletonList("getConnection"));
+//			put("java.sql.DriverManager", Collections.singletonList("getConnection"));
 //			put("com.mysql.jdbc.ConnectionImpl", Arrays.asList("getInstance", "isReadOnly"));
 		}
 	};
