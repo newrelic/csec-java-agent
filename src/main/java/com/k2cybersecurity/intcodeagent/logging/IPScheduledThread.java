553d4710c5e40f62e74605783cc23c38cd1e20b8
anupam-ap <anupam@k2io.com>
2019-05-28 17:52:04 +0530

    fixes in IPScheduledThread.java

--- src/main/java/com/k2cybersecurity/intcodeagent/logging/IPScheduledThread.java
+++ src/main/java/com/k2cybersecurity/intcodeagent/logging/IPScheduledThread.java
@@ -2,6 +2,7 @@ package com.k2cybersecurity.intcodeagent.logging;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.net.Socket;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
@@ -17,21 +18,21 @@ public class IPScheduledThread {
 	private IPScheduledThread() {
 		Runnable runnable = new Runnable() {
 			public void run() {
+				Socket socket = EventThreadPool.getInstance().getSocket();
 				try (BufferedReader reader = new BufferedReader(new FileReader("/etc/k2-adp/hostip.properties"))) {
 					String hostip = reader.readLine();
-					
 					try {
 					System.out.println("Host ip equals : " + LoggingInterceptor.hostip.equals(hostip));
-					System.out.println("LoggingInterceptor.socket : " + LoggingInterceptor.socket);
-					System.out.println("LoggingInterceptor.socket.isConnected() : " + LoggingInterceptor.socket.isConnected());
-					System.out.println("LoggingInterceptor.socket.isClosed() : " + LoggingInterceptor.socket.isClosed());
+					System.out.println("LoggingInterceptor.socket : " + socket);
+					System.out.println("LoggingInterceptor.socket.isConnected() : " + socket.isConnected());
+					System.out.println("LoggingInterceptor.socket.isClosed() : " + socket.isClosed());
 					} catch (Exception ex) {
 						ex.printStackTrace();
 					}
 					if (hostip == null || hostip.equals("")) {
 						System.out.println("Host ip not found");
-					} else if (!LoggingInterceptor.hostip.equals(hostip) || (LoggingInterceptor.socket == null
-							|| !LoggingInterceptor.socket.isConnected() || LoggingInterceptor.socket.isClosed())) {
+					} else if (!LoggingInterceptor.hostip.equals(hostip) || (socket == null
+							|| !socket.isConnected() || socket.isClosed())) {
 						LoggingInterceptor.connectSocket();
 						LoggingInterceptor.createApplicationInfoBean();
 						LoggingInterceptor.getJarPath();
