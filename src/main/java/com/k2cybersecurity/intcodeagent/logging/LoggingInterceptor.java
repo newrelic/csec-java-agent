ae7cb2a596b44500b5bd737bd4c43331db9b00f0
Anmol Jain <anmol@k2io.com>
2019-05-29 18:21:27 +0530

    launch ip thread before connect socket

--- src/main/java/com/k2cybersecurity/intcodeagent/logging/LoggingInterceptor.java
+++ src/main/java/com/k2cybersecurity/intcodeagent/logging/LoggingInterceptor.java
@@ -214,11 +214,11 @@ public class LoggingInterceptor extends Interceptor {
 		 * RuntimeException(ex); }
 		 */
 		try {
+			IPScheduledThread.getInstance();
 			connectSocket();
 			getJarPath();
 			createApplicationInfoBean();
 			eventWritePool();
-			IPScheduledThread.getInstance();
 			System.out.println("K2-JavaAgent installed successfully.");
 
 		} catch (Exception e) {
