54c2c27845170f3bf22b79d3dc13369e220e42cf
Harshit Singh Lodha <harshit@k2io.com>
2019-08-28 16:44:59 +0530

    Added logging for allClassLoadersCount

--- src/main/java/com/k2cybersecurity/intcodeagent/websocket/WSClient.java
+++ src/main/java/com/k2cybersecurity/intcodeagent/websocket/WSClient.java
@@ -16,56 +16,57 @@ import com.k2cybersecurity.intcodeagent.models.javaagent.IntCodeControlCommand;
 
 public class WSClient extends WebSocketClient {
 
-	private static final FileLoggerThreadPool logger = FileLoggerThreadPool.getInstance();
+    private static final FileLoggerThreadPool logger = FileLoggerThreadPool.getInstance();
 
-	private static WSClient instance;
+    private static WSClient instance;
 
-	private WSClient() throws URISyntaxException, InterruptedException {
-		super(new URI(String.format("ws://%s:%s", LoggingInterceptor.hostip, 54321)));
-		logger.log(LogLevel.INFO, "Creating WSock connection to : "+ LoggingInterceptor.hostip, WSClient.class.getName());
-		if (!connectBlocking()) {
-			logger.log(LogLevel.SEVERE, "WSock connection to "+LoggingInterceptor.hostip +" failed", WSClient.class.getName());
-		}
-	}
+    private WSClient() throws URISyntaxException, InterruptedException {
+        super(new URI(String.format("ws://%s:%s", LoggingInterceptor.hostip, 54321)));
+        logger.log(LogLevel.INFO, "Creating WSock connection to : " + LoggingInterceptor.hostip, WSClient.class.getName());
+        if (!connectBlocking()) {
+            logger.log(LogLevel.SEVERE, "WSock connection to " + LoggingInterceptor.hostip + " failed", WSClient.class.getName());
+        }
+    }
 
-	@Override
-	public void onOpen(ServerHandshake handshakedata) {
-		logger.log(LogLevel.INFO, "Opened WSock to "+ this.getRemoteSocketAddress(), WSClient.class.getName());
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        logger.log(LogLevel.INFO, "Opened WSock to " + this.getRemoteSocketAddress(), WSClient.class.getName());
 //		logger.log(LogLevel.INFO, "Current WSock ready status : {0},{1},{2}",
 //				new Object[] { this.isOpen(), this.isClosing(), this.isClosed() });
-		super.send(LoggingInterceptor.APPLICATION_INFO_BEAN.toString());
-		Agent.allClassLoadersCount.set(0);
-		logger.log(LogLevel.INFO, "Application info posted : "+ LoggingInterceptor.APPLICATION_INFO_BEAN, WSClient.class.getName());
-	}
+        super.send(LoggingInterceptor.APPLICATION_INFO_BEAN.toString());
+        Agent.allClassLoadersCount.set(0);
+        logger.log(LogLevel.INFO, "Resetting allClassLoadersCount to " + Agent.allClassLoadersCount.get(), WSClient.class.getName());
+        logger.log(LogLevel.INFO, "Application info posted : " + LoggingInterceptor.APPLICATION_INFO_BEAN, WSClient.class.getName());
+    }
 
-	@Override
-	public void onMessage(String message) {
-		// TODO : Receive communication from IC side.
-		//		logger.log(Level.FINE, "Message from IC : {0}", message);
-		try {
-			IntCodeControlCommand controlCommand = new ObjectMapper().readValue(message, IntCodeControlCommand.class);
-			AgentUtils.controlCommandProcessor(controlCommand);
-		} catch (Exception e) {
-			logger.log(LogLevel.SEVERE, "Unable to process incoming message : "+message+" : due to error : "+ e, WSClient.class.getName());
-		}
-	}
+    @Override
+    public void onMessage(String message) {
+        // TODO : Receive communication from IC side.
+        //		logger.log(Level.FINE, "Message from IC : {0}", message);
+        try {
+            IntCodeControlCommand controlCommand = new ObjectMapper().readValue(message, IntCodeControlCommand.class);
+            AgentUtils.controlCommandProcessor(controlCommand);
+        } catch (Exception e) {
+            logger.log(LogLevel.SEVERE, "Unable to process incoming message : " + message + " : due to error : " + e, WSClient.class.getName());
+        }
+    }
 
-	@Override
-	public void onClose(int code, String reason, boolean remote) {
-		logger.log(LogLevel.WARNING, "Connection closed by " + (remote ? "remote peer." : "local.") + " Code: " + code
-				+ " Reason: " + reason, WSClient.class.getName());
-	}
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        logger.log(LogLevel.WARNING, "Connection closed by " + (remote ? "remote peer." : "local.") + " Code: " + code
+                + " Reason: " + reason, WSClient.class.getName());
+    }
 
-	@Override
-	public void onError(Exception ex) {
-		logger.log(LogLevel.SEVERE, "Error in WSock connection : " + ex.getMessage() + " : " + ex.getCause(), WSClient.class.getName());
-	}
+    @Override
+    public void onError(Exception ex) {
+        logger.log(LogLevel.SEVERE, "Error in WSock connection : " + ex.getMessage() + " : " + ex.getCause(), WSClient.class.getName());
+    }
 
-	@Override
-	public void send(String text) {
-		if (this.isOpen()) {
-			super.send(text);
-		} else {
+    @Override
+    public void send(String text) {
+        if (this.isOpen()) {
+            super.send(text);
+        } else {
 //			try {
 //				if (this.reconnectBlocking()) {
 //					super.send(text);
@@ -76,43 +77,43 @@ public class WSClient extends WebSocketClient {
 //			} catch (URISyntaxException | InterruptedException e) {
 //				logger.log(LogLevel.SEVERE, "Error in WSock reconnection : " + e.getMessage() + " : " + e.getCause());
 //			}
-			logger.log(LogLevel.WARNING, "Unable to send event : "+ text, WSClient.class.getName());
-		}
-	}
+            logger.log(LogLevel.WARNING, "Unable to send event : " + text, WSClient.class.getName());
+        }
+    }
 
-	/**
-	 * @return the instance
-	 * @throws URISyntaxException
-	 * @throws InterruptedException
-	 */
-	public static WSClient getInstance() throws URISyntaxException, InterruptedException {
-		if (instance == null) {
-			instance = new WSClient();
-		}
-		return instance;
-	}
+    /**
+     * @return the instance
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     */
+    public static WSClient getInstance() throws URISyntaxException, InterruptedException {
+        if (instance == null) {
+            instance = new WSClient();
+        }
+        return instance;
+    }
 
-	/**
-	 * @return the instance
-	 * @throws URISyntaxException
-	 * @throws InterruptedException
-	 */
-	public static WSClient reconnectWSClient() throws URISyntaxException, InterruptedException {
-		boolean reconnectStatus = false;
-		if (instance != null) {
-			instance.closeBlocking();
-			try {
-				reconnectStatus = instance.reconnectBlocking();
-			} catch (Exception e) {
-				reconnectStatus = false;
-			}
-		}
-		if (!reconnectStatus) {
-			if (instance !=null) {
-				instance.closeBlocking();
-			}
-			instance = new WSClient();
-		}
-		return instance;
-	}
+    /**
+     * @return the instance
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     */
+    public static WSClient reconnectWSClient() throws URISyntaxException, InterruptedException {
+        boolean reconnectStatus = false;
+        if (instance != null) {
+            instance.closeBlocking();
+            try {
+                reconnectStatus = instance.reconnectBlocking();
+            } catch (Exception e) {
+                reconnectStatus = false;
+            }
+        }
+        if (!reconnectStatus) {
+            if (instance != null) {
+                instance.closeBlocking();
+            }
+            instance = new WSClient();
+        }
+        return instance;
+    }
 }
