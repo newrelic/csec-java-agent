9fdd2aa6ecf28474d795a7aebe7c627f2c53a3d0
lovesh-ap <lovesh@k2io.com>
2019-08-28 16:51:56 +0530

    File logging rollover functionality

--- src/main/java/com/k2cybersecurity/intcodeagent/filelogging/LogWriter.java
+++ src/main/java/com/k2cybersecurity/intcodeagent/filelogging/LogWriter.java
@@ -3,14 +3,25 @@ package com.k2cybersecurity.intcodeagent.filelogging;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import org.apache.commons.lang3.StringUtils;
 import org.brutusin.instrumentation.Agent;
 
 import com.k2cybersecurity.intcodeagent.properties.K2JALogProperties;
 
 public class LogWriter implements Runnable {
+
+	private static final String STRING_DOT = ".";
+
+	private static final String STR_HYPHEN = " - ";
+
+	private static final String STR_COLON = " : ";
+
+	private static final String K2_LOG = "K2-LOG : ";
 
 	private static int defaultLogLevel;
 
@@ -21,7 +32,7 @@ public class LogWriter implements Runnable {
 	private String logEntry;
 
 	private String loggingClassName;
-	
+
 	private static long maxFileSize;
 
 	Calendar cal = Calendar.getInstance();
@@ -29,21 +40,28 @@ public class LogWriter implements Runnable {
 
 	public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
 
-	private static String fileName;
-	
-	private static String updatedFileName;
-	
+	private static final String fileName;
+
+	private static final String currentLogFileName;
+
 	private static int logFileCounter = 0;
+
+	private static BufferedWriter writer;
+
+	private static final File currentLogFile;
 
 	static {
 		fileName = "/etc/k2-adp/logs/k2_java_agent-" + Agent.APPLICATION_UUID + ".log";
-		updatedFileName = fileName;
+		currentLogFileName = fileName + STRING_DOT + logFileCounter;
+
+		currentLogFile = new File(currentLogFileName);
 		try {
+			writer = new BufferedWriter(new FileWriter(currentLogFileName, true));
 			maxFileSize = K2JALogProperties.maxfilesize * 1048576;
-			
-			//k2.log.handler.maxfilesize=10
-			//k2.log.handler.maxfilesize.unit=MB
-			
+
+			// k2.log.handler.maxfilesize=10
+			// k2.log.handler.maxfilesize.unit=MB
+
 			String level = K2JALogProperties.level;
 			if (level.equals("OFF")) {
 				defaultLogLevel = LogLevel.OFF.getLevel();
@@ -78,27 +96,44 @@ public class LogWriter implements Runnable {
 			return;
 		}
 		StringBuilder sb = new StringBuilder();
-		sb.append("K2-LOG : ");
+		sb.append(K2_LOG);
 		sb.append(sdf.format(cal.getTime()));
-		sb.append(" : ");
+		sb.append(STR_COLON);
 		sb.append(this.logLevelName);
 		if (this.loggingClassName != null)
-			sb.append(" : ");
+			sb.append(STR_COLON);
 		sb.append(this.loggingClassName);
-		sb.append(" - ");
+		sb.append(STR_HYPHEN);
 		sb.append(this.logEntry);
-		File file = new File(updatedFileName);
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(updatedFileName, true))) {
+		try {
 			writer.write(sb.toString());
 			writer.newLine();
-			if(file.length()> maxFileSize) {
-				logFileCounter++;
-				updatedFileName= fileName + "." + logFileCounter;
-			}
-		} catch (Throwable e) {
-			e.printStackTrace();
+			rollover(currentLogFile);
+		} catch (IOException e) {
 		}
 
+	}
+
+	private static void rollover(File currentFile) throws IOException {
+		if (currentFile.length() > maxFileSize) {
+			writer.close();
+			logFileCounter++;
+			File rolloverFile = new File(fileName + STRING_DOT + logFileCounter);
+			currentFile.renameTo(rolloverFile);
+
+			PrintWriter pw = new PrintWriter(new File(currentLogFileName));
+			pw.write(StringUtils.EMPTY);
+			pw.close();
+
+			writer = new BufferedWriter(new FileWriter(currentLogFileName, true));
+			
+			int removeFile = logFileCounter - K2JALogProperties.maxfiles;
+			if (removeFile > 0) {
+				File remove = new File(fileName + STRING_DOT + removeFile);
+				if (remove.exists())
+					remove.delete();
+			}
+		}
 	}
 
 }
