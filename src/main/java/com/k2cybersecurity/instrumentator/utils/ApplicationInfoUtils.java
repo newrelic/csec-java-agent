bd0cbfaf5dc34736a35d981192425900aa25b0ca
Harshit Singh Lodha <harshit@k2io.com>
2020-01-22 18:28:11 +0530

    getDefaultGateway updated

--- src/main/java/com/k2cybersecurity/instrumentator/utils/ApplicationInfoUtils.java
+++ src/main/java/com/k2cybersecurity/instrumentator/utils/ApplicationInfoUtils.java
@@ -1,29 +1,26 @@
 package com.k2cybersecurity.instrumentator.utils;
 
-import static com.k2cybersecurity.intcodeagent.logging.IAgentConstants.CMD_LINE_DIR;
-import static com.k2cybersecurity.intcodeagent.logging.IAgentConstants.PROC_DIR;
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.Socket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-
 import com.k2cybersecurity.instrumentator.K2Instrumentator;
 import com.k2cybersecurity.intcodeagent.logging.DeployedApplication;
 import com.k2cybersecurity.intcodeagent.websocket.EventSendPool;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Set;
+
+import static com.k2cybersecurity.intcodeagent.logging.IAgentConstants.PROC_DIR;
 
 public class ApplicationInfoUtils {
+
+	public static final String SELF_NET_ROUTE = "self/net/route";
+	public static final String CS_1 = "00000000";
+	public static final String DOT = ".";
 
 	public static void updateServerInfo() {
 		Set<DeployedApplication> deployedApplications = getAllDeployedApplications();
@@ -44,10 +41,10 @@ public class ApplicationInfoUtils {
 
 	public static String getDefaultGetway() throws IOException {
 		try {
-			List<String> routes = IOUtils.readLines(new FileInputStream(new File(PROC_DIR + "self/route")));
+			List<String> routes = IOUtils.readLines(new FileInputStream(new File(PROC_DIR + SELF_NET_ROUTE)));
 			for(int i=1; i<routes.size(); i++) {
 				String[] route = routes.get(i).split("\\s+");
-				if(StringUtils.equals("0000000",route[1])) {
+				if(StringUtils.equals(CS_1,route[1])) {
 					return getDefaultGetway(route[2]);
 				}
 			}
@@ -59,15 +56,14 @@ public class ApplicationInfoUtils {
 	}
 
 	private static String getDefaultGetway(String hexGateway) {
-		hexGateway = StringUtils.reverse(hexGateway);
-		
+
 		StringBuilder gateway = new StringBuilder();
-		for(int i=0; i<hexGateway.length(); i+=2) {
+		for(int i=hexGateway.length()-2; i>=0; i-=2) {
 			String hex = StringUtils.substring(hexGateway, i, i+2);
 			gateway.append(Integer.parseInt(hex, 16));
-			gateway.append(".");
+			gateway.append(DOT);
 		}
-		return StringUtils.removeEnd(gateway.toString(), ".");
+		return StringUtils.removeEnd(gateway.toString(), DOT);
 	}
 
 	public static void main(String[] args) throws IOException {
