0a63dba14983870d20b2a9a96ce2a4a5f1517d26
Harshit Singh Lodha <harshit@k2io.com>
2020-01-29 16:07:52 +0530

    Loggers for debugging.

--- src/main/java/com/k2cybersecurity/instrumentator/dispatcher/Dispatcher.java
+++ src/main/java/com/k2cybersecurity/instrumentator/dispatcher/Dispatcher.java
@@ -251,9 +251,9 @@ public class Dispatcher implements Runnable {
 		}
 		eventBean.setEventGenerationTime(Instant.now().toEpochMilli());
 		EventSendPool.getInstance().sendEvent(eventBean.toString());
-//		System.out.println("============= Event Start ============");
-//		System.out.println(eventBean);
-//		System.out.println("============= Event End ============");
+		System.out.println("============= Event Start ============");
+		System.out.println(eventBean);
+		System.out.println("============= Event End ============");
 	}
 
 	private JavaAgentEventBean prepareXPATHEvent(JavaAgentEventBean eventBean,
