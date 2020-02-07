d8e93c909653fcf6d85efe55e3ab37deb64e5425
Harshit Singh Lodha <harshit@k2io.com>
2020-02-07 16:09:56 +0530

    Start getting response back in collector.

---
+++ src/main/java/com/k2cybersecurity/intcodeagent/models/javaagent/EventResponse.java
@@ -0,0 +1,43 @@
+package com.k2cybersecurity.intcodeagent.models.javaagent;
+
+public class EventResponse {
+
+	private String id;
+
+	private String jsonName = "EventResponse";
+
+	private boolean attack;
+
+	private String resultMessage;
+
+	public EventResponse() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getJsonName() {
+		return jsonName;
+	}
+
+	public boolean isAttack() {
+		return attack;
+	}
+
+	public void setAttack(boolean attack) {
+		this.attack = attack;
+	}
+
+	public String getResultMessage() {
+		return resultMessage;
+	}
+
+	public void setResultMessage(String resultMessage) {
+		this.resultMessage = resultMessage;
+	}
+}
