9ee9799f7f421f84d0e2ed33693e81432775f718
Harshit Singh Lodha <harshit@k2io.com>
2020-02-10 17:00:49 +0530

    Temporary log level change.

--- src/main/java/com/k2cybersecurity/instrumentator/dispatcher/EventDispatcher.java
+++ src/main/java/com/k2cybersecurity/instrumentator/dispatcher/EventDispatcher.java
@@ -153,7 +153,7 @@ public class EventDispatcher {
 		try {
 			eventResponse.getResponseSemaphore().acquire();
 			if(eventResponse.getResponseSemaphore().tryAcquire(100, TimeUnit.MILLISECONDS)){
-				logger.log(LogLevel.DEBUG,
+				logger.log(LogLevel.INFO,
 							EVENT_RESPONSE_TIME_TAKEN + eventResponse.getEventId() + DOUBLE_COLON_SEPERATOR + (
 									eventResponse.getReceivedTime() - eventResponse.getGenerationTime()), EventDispatcher.class.getSimpleName());
 				return true;
