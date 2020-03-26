4a8fc89766f6018fa69ebfbce711eaf59442c938
Lovesh Baya <lovesh@k2io.com>
2020-03-26 09:52:28 +0530

    hash code and equals for cve components

--- src/main/java/com/k2cybersecurity/intcodeagent/models/javaagent/CVEComponent.java
+++ src/main/java/com/k2cybersecurity/intcodeagent/models/javaagent/CVEComponent.java
@@ -50,5 +50,36 @@ public class CVEComponent {
 	public String toString() {
 		return JsonConverter.toJSON(this);
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sha256 == null) ? 0 : sha256.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CVEComponent other = (CVEComponent) obj;
+		if (sha256 == null) {
+			if (other.sha256 != null)
+				return false;
+		} else if (!sha256.equals(other.sha256))
+			return false;
+		return true;
+	}
 	
 }
