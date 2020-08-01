895041c37bfaa4289d32503e8620becf0333e501
Harshit Singh Lodha <harshit@k2io.com>
2020-08-01 16:51:01 +0530

    Bug fix for File Integrity not being flagged for a case where there are multiple file calls in the downstream.

--- src/main/java/com/k2cybersecurity/instrumentator/decorators/fileaccess/Callbacks.java
+++ src/main/java/com/k2cybersecurity/instrumentator/decorators/fileaccess/Callbacks.java
@@ -63,7 +63,7 @@ public class Callbacks {
 	private static FileIntegrityBean createEntryOfFileIntegrity(String fileName, String sourceString, String className, String methodName, String exectionId) {
 		File file = Paths.get(fileName).toFile();
 		String extension = FilenameUtils.getExtension(fileName);
-		if (SOURCE_EXENSIONS.contains(extension)) {
+		if (SOURCE_EXENSIONS.contains(extension) && !ThreadLocalExecutionMap.getInstance().getFileLocalMap().containsKey(fileName)) {
 			FileIntegrityBean fbean = new FileIntegrityBean(file.exists(), fileName, className,
 					sourceString, exectionId, Instant.now().toEpochMilli(), methodName);
 			ThreadLocalExecutionMap.getInstance().getFileLocalMap().put(fileName,
