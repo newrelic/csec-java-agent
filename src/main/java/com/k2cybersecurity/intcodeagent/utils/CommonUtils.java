f9154040ea54472c0db89d5a56da9267b49ece16
Harshit Singh Lodha <harshit@k2io.com>
2022-08-24 17:13:52 +0530

    Revert "Fix CVEs commons-beanutils v1.9.2 & com.google.guava v19.0"
    This reverts commit 0f0471f0cf18526e127a312995dc5ca0795bfd13.

--- src/main/java/com/k2cybersecurity/intcodeagent/utils/CommonUtils.java
+++ src/main/java/com/k2cybersecurity/intcodeagent/utils/CommonUtils.java
@@ -13,11 +13,12 @@ import com.k2cybersecurity.intcodeagent.filelogging.LogLevel;
 import com.k2cybersecurity.intcodeagent.models.config.AgentPolicy;
 import com.k2cybersecurity.intcodeagent.models.config.AgentPolicyParameters;
 import com.k2cybersecurity.intcodeagent.schedulers.PolicyPullST;
-import com.networknt.schema.JsonSchema;
-import com.networknt.schema.JsonSchemaFactory;
-import com.networknt.schema.SpecVersion;
-import com.networknt.schema.ValidationMessage;
 import org.apache.commons.io.FileUtils;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,7 +28,6 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.Stack;
 
 public class CommonUtils {
@@ -35,23 +35,21 @@ public class CommonUtils {
     private static final FileLoggerThreadPool logger = FileLoggerThreadPool.getInstance();
     public static final String POLICY_WRITE_FAILED = "policy write failed : ";
     public static final String POLICY_WRITTEN_TO_FILE = "policy written to file : ";
-    public static JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4);
-    public static JsonSchema lcPolicyParamSchema = factory.getSchema(CommonUtils.class.getClassLoader().getSystemResourceAsStream("lc-policy-parameters-schema.json"));
-    public static JsonSchema lcPolicySchema = factory.getSchema(CommonUtils.class.getClassLoader().getSystemResourceAsStream("lc-policy-schema.json"));
-
-    static {
-        lcPolicySchema.initializeValidators();
-        lcPolicyParamSchema.initializeValidators();
-    }
 
     public static boolean validateCollectorPolicyParameterSchema(AgentPolicyParameters policyParameters) {
+
         try {
-            Set<ValidationMessage> errors = lcPolicyParamSchema.validate(new ObjectMapper().readTree(policyParameters.toString()));
-            if (errors.isEmpty()) {
-                return true;
-            }
-            logger.log(LogLevel.ERROR, String.format("LC Policy Parameters validation failed due to following violations: %s", errors), CommonUtils.class.getName());
-            return false;
+            JSONObject jsonSchema = new JSONObject(
+                    new JSONTokener(CommonUtils.class.getClassLoader().getSystemResourceAsStream("lc-policy-parameters-schema.json")));
+            JSONObject jsonSubject = new JSONObject(
+                    new JSONTokener(policyParameters.toString()));
+
+            Schema schema = SchemaLoader.load(jsonSchema);
+            schema.validate(jsonSubject);
+            return true;
+        } catch (ValidationException e) {
+            logger.log(LogLevel.ERROR, String.format("LC Policy Parameters validation failed due to following violations: %s", e.getAllMessages()), CommonUtils.class.getName());
+            logger.log(LogLevel.DEBUG, "LC Policy Parameters validation failed due to", e, CommonUtils.class.getName());
         } catch (Exception e) {
             logger.log(LogLevel.ERROR, String.format("Exception raised in LC policy Parameters validation : %s :: caused by : %s", e.getMessage(), e.getCause()), CommonUtils.class.getName());
             logger.log(LogLevel.DEBUG, "Exception raised in LC policy Parameters validation", e, CommonUtils.class.getName());
@@ -59,36 +57,20 @@ public class CommonUtils {
         return false;
     }
 
-//    public static boolean validateCollectorPolicyParameterSchema(AgentPolicyParameters policyParameters) {
-//
-//        try {
-//            JSONObject jsonSchema = new JSONObject(
-//                    new JSONTokener(CommonUtils.class.getClassLoader().getSystemResourceAsStream("lc-policy-parameters-schema.json")));
-//            JSONObject jsonSubject = new JSONObject(
-//                    new JSONTokener(policyParameters.toString()));
-//
-//            Schema schema = SchemaLoader.load(jsonSchema);
-//            schema.validate(jsonSubject);
-//            return true;
-//        } catch (ValidationException e) {
-//            logger.log(LogLevel.ERROR, String.format("LC Policy Parameters validation failed due to following violations: %s", e.getAllMessages()), CommonUtils.class.getName());
-//            logger.log(LogLevel.DEBUG, "LC Policy Parameters validation failed due to", e, CommonUtils.class.getName());
-//        } catch (Exception e) {
-//            logger.log(LogLevel.ERROR, String.format("Exception raised in LC policy Parameters validation : %s :: caused by : %s", e.getMessage(), e.getCause()), CommonUtils.class.getName());
-//            logger.log(LogLevel.DEBUG, "Exception raised in LC policy Parameters validation", e, CommonUtils.class.getName());
-//        }
-//        return false;
-//    }
-
     public static boolean validateCollectorPolicySchema(AgentPolicy policy) {
 
         try {
-            Set<ValidationMessage> errors = lcPolicySchema.validate(new ObjectMapper().readTree(policy.toString()));
-            if (errors.isEmpty()) {
-                return true;
-            }
-            logger.log(LogLevel.ERROR, String.format("LC Policy Parameters validation failed due to following violations: %s", errors), CommonUtils.class.getName());
-            return false;
+            JSONObject jsonSchema = new JSONObject(
+                    new JSONTokener(CommonUtils.class.getClassLoader().getSystemResourceAsStream("lc-policy-schema.json")));
+            JSONObject jsonSubject = new JSONObject(
+                    new JSONTokener(policy.toString()));
+
+            Schema schema = SchemaLoader.load(jsonSchema);
+            schema.validate(jsonSubject);
+            return true;
+        } catch (ValidationException e) {
+            logger.log(LogLevel.ERROR, String.format("LC Policy validation failed due to following violations: %s", e.getAllMessages()), CommonUtils.class.getName());
+            logger.log(LogLevel.DEBUG, "LC Policy validation failed due to", e, CommonUtils.class.getName());
         } catch (Exception e) {
             logger.log(LogLevel.ERROR, String.format("Exception raised in LC policy validation : %s :: caused by : %s", e.getMessage(), e.getCause()), CommonUtils.class.getName());
             logger.log(LogLevel.DEBUG, "Exception raised in LC policy validation", e, CommonUtils.class.getName());
