c9d5457dd16e0628fe607ff37a2ddabee65f6292
Harshit Singh Lodha <harshit@k2io.com>
2022-11-04 18:14:40 +0530

    Fix in status file policy version

--- src/main/java/com/k2cybersecurity/intcodeagent/schedulers/PolicyPullST.java
+++ src/main/java/com/k2cybersecurity/intcodeagent/schedulers/PolicyPullST.java
@@ -143,11 +143,11 @@ public class PolicyPullST {
             AgentUtils.getInstance().setApplicationInfo();
             if (AgentUtils.getInstance().isPolicyOverridden()){
                 AgentUtils.getInstance().getAgentPolicy().setVersion("overridden");
-                AgentUtils.getInstance().getStatusLogValues().put("policy-version", AgentUtils.getInstance().getAgentPolicy().getVersion());
                 logger.log(LogLevel.INFO, String.format("NR policy over-ride in place. Updated policy : %s",
                         AgentUtils.getInstance().getAgentPolicy()), PolicyPullST.class.getName());
                 CommonUtils.fireUpdatePolicyAPI(AgentUtils.getInstance().getAgentPolicy());
             }
+            AgentUtils.getInstance().getStatusLogValues().put("policy-version", AgentUtils.getInstance().getAgentPolicy().getVersion());
             EventSendPool.getInstance().sendEvent(K2Instrumentator.APPLICATION_INFO_BEAN.toString());
             return true;
         } catch (Throwable e) {
