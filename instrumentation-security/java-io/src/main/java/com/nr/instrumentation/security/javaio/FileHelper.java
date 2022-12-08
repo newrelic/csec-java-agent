627d28ece3641dfb750fe509b60b588abaf558c0
Harshit Singh Lodha <harshit@k2io.com>
2022-12-08 18:12:04 +0530

    File Stream hooks : Change to exit events.

---
+++ instrumentation-security/java-io/src/main/java/com/nr/instrumentation/security/javaio/FileHelper.java
@@ -0,0 +1,58 @@
+package com.nr.instrumentation.security.javaio;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.operation.FileIntegrityOperation;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileHelper {
+
+    public static final String FILEOUTPUTSTREAM_OPEN = "open";
+
+    public static final List<String> ALLOWED_EXTENSIONS = Arrays.asList(new String[]{"css", "html", "htm", "jsp", "js", "classtmp"});
+
+    public static final List<String> SOURCE_EXENSIONS = Arrays.asList(new String[]{"class", "jsp", "jar", "java"});
+
+
+    public static boolean skipExistsEvent(String filename) {
+        String extension = getFileExtension(filename);
+        if (!(NewRelicSecurity.getAgent().getCurrentPolicy().getVulnerabilityScan().getEnabled() &&
+                NewRelicSecurity.getAgent().getCurrentPolicy().getVulnerabilityScan().getIastScan().getEnabled()) &&
+                extension != null && !extension.trim().isEmpty() &&
+                (SOURCE_EXENSIONS.contains(extension) || ALLOWED_EXTENSIONS.contains(extension))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static String getFileExtension(File file) {
+        String fileName = file.getName();
+        return getFileExtension(fileName);
+    }
+
+    public static String getFileExtension(String fileName) {
+        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
+        else
+            return "";
+    }
+
+    public static FileIntegrityOperation createEntryOfFileIntegrity(String fileName, String className, String methodName) {
+        File file = Paths.get(fileName).toFile();
+        String extension = getFileExtension(file);
+        if (SOURCE_EXENSIONS.contains(extension) &&
+                !NewRelicSecurity.getAgent().getSecurityMetaData().getFileLocalMap().containsKey(fileName)) {
+            FileIntegrityOperation fbean = new FileIntegrityOperation(file.exists(), fileName, className,
+                    methodName);
+            NewRelicSecurity.getAgent().getSecurityMetaData().getFileLocalMap().put(fileName,
+                    fbean);
+            return fbean;
+        }
+        return null;
+    }
+
+}
---
+++ instrumentation-security/java-io/src/main/java/java/io/RandomAccessFile_Instrumentation.java
@@ -0,0 +1,60 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package java.io;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.VulnerabilityCaseType;
+import com.newrelic.api.agent.security.schema.exceptions.NewRelicSecurityException;
+import com.newrelic.api.agent.security.schema.operation.FileOperation;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.security.javaio.FileHelper;
+
+@Weave(type = MatchType.ExactClass, originalName = "java.io.RandomAccessFile")
+public abstract class RandomAccessFile_Instrumentation {
+
+    private void open(String name, int mode) throws FileNotFoundException {
+        AbstractOperation operation = preprocessSecurityHook(name);
+        Weaver.callOriginal();
+        registerExitOperation(operation);
+    }
+
+    private void registerExitOperation(AbstractOperation operation) {
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive() || NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()
+            ) {
+                return;
+            }
+            NewRelicSecurity.getAgent().registerExitEvent(operation);
+        } catch (Throwable ignored){}
+    }
+
+    private AbstractOperation preprocessSecurityHook(String filename) {
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive() || NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()
+                    || filename == null || filename.trim().isEmpty()
+            ) {
+                return null;
+            }
+            String filePath = new File(filename).getAbsolutePath();
+            FileOperation operation = new FileOperation(filePath,
+                    FileOutputStream_Instrumentation.class.getName(), FileHelper.FILEOUTPUTSTREAM_OPEN, false);
+            FileHelper.createEntryOfFileIntegrity(filePath, FileOutputStream_Instrumentation.class.getName(), FileHelper.FILEOUTPUTSTREAM_OPEN);
+            NewRelicSecurity.getAgent().registerOperation(operation);
+            return operation;
+        } catch (Throwable e) {
+            if (e instanceof NewRelicSecurityException) {
+                e.printStackTrace();
+                throw e;
+            }
+        }
+        return null;
+    }
+}
