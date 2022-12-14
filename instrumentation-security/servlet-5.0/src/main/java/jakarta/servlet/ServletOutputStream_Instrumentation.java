fb605e9f9595b885f742a95f8d0712eac57f143e
Harshit Singh Lodha <harshit@k2io.com>
2022-12-14 16:46:30 +0530

    Initial support for Jakarta Servlet 5.0

---
+++ instrumentation-security/servlet-5.0/src/main/java/jakarta/servlet/ServletOutputStream_Instrumentation.java
@@ -0,0 +1,368 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package jakarta.servlet;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.security.servlet5.ServletResponseCallback;
+
+import java.io.IOException;
+
+@Weave(type = MatchType.BaseClass, originalName = "jakarta.servlet.ServletOutputStream")
+public abstract class ServletOutputStream_Instrumentation {
+    @NewField
+    public Boolean servletOutputStreamDataGatheringAllowed;
+
+    @NewField
+    public boolean cascadedCall;
+
+    protected ServletOutputStream_Instrumentation(){}
+
+    public void write(int b) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append((char) b);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+
+    public void print(String o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+
+    public void print(boolean o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void print(char o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void print(int o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void print(long o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void print(float o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void print(double o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void println() throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(ServletResponseCallback.LF);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void println(String o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(ServletResponseCallback.LF);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void println(boolean o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(ServletResponseCallback.LF);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void println(char o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(ServletResponseCallback.LF);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void println(int o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(ServletResponseCallback.LF);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void println(long o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(ServletResponseCallback.LF);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void println(float o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(ServletResponseCallback.LF);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+    public void println(double o) throws IOException {
+        boolean currentCascadedCall = cascadedCall;
+
+        // Preprocess Phase
+        if(preprocessSecurityHook(currentCascadedCall)){
+            try {
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(o);
+                NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().getBody().append(ServletResponseCallback.LF);
+            } catch (Throwable ignored) {
+//                ignored.printStackTrace(System.out);
+            }
+        }
+
+        // Actual Call
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessSecurityHook(currentCascadedCall);
+        }
+    }
+
+    private boolean preprocessSecurityHook(boolean currentCascadedCall) {
+        try {
+            if(Boolean.FALSE.equals(servletOutputStreamDataGatheringAllowed) ||
+                    !NewRelicSecurity.isHookProcessingActive()) {
+                return false;
+            }
+//                System.out.println("Start IS2 "+ this.hashCode());
+            if(servletOutputStreamDataGatheringAllowed == null) {
+                servletOutputStreamDataGatheringAllowed = ServletResponseCallback.processResponseOutputStreamHookData(this.hashCode());
+            }
+
+            if (Boolean.TRUE.equals(servletOutputStreamDataGatheringAllowed) && !currentCascadedCall) {
+                cascadedCall = true;
+                return true;
+//                        System.out.println("Writing from IS 2" + this.hashCode() + " : " + String.valueOf(data));
+            }
+        } catch(Throwable ignored) {
+//            ignored.printStackTrace();
+        }
+        return false;
+    }
+
+    private void postProcessSecurityHook(boolean currentCascadedCall) {
+        cascadedCall = currentCascadedCall;
+    }
+}
