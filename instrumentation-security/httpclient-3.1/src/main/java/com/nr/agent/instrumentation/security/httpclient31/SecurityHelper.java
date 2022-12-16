98140f019a5352423f79da4109ea176bb9f40966
Harshit Singh Lodha <harshit@k2io.com>
2022-12-16 16:33:24 +0530

    Initial inst module for Apache commons httpclient 3.1
    Co-authored-by: Lovesh Baya <lbaya@newrelic.com>

---
+++ instrumentation-security/httpclient-3.1/src/main/java/com/nr/agent/instrumentation/security/httpclient31/SecurityHelper.java
@@ -0,0 +1,26 @@
+package com.nr.agent.instrumentation.security.httpclient31;
+
+public class SecurityHelper {
+
+    public static final String METHOD_NAME_EXECUTE = "execute";
+    public static final String NULL_STRING = "null";
+
+    public static String getURI(String scheme, String host, int port, String path) {
+        StringBuilder sb = new StringBuilder();
+        if (scheme != null) {
+            sb.append(scheme);
+            sb.append("://");
+        }
+        if (host != null) {
+            sb.append(host);
+            if (port >= 0) {
+                sb.append(":");
+                sb.append(port);
+            }
+        }
+        if (path != null) {
+            sb.append(path);
+        }
+        return sb.toString();
+    }
+}
