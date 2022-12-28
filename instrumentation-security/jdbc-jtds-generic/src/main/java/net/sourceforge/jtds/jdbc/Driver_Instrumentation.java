f2eecf3bb0d1f06e310754260d7323976fdd44b9
lovesh-ap <lbaya@newrelic.com>
2022-12-28 10:33:52 +0530

    Instrumentation adopt for jtds-generic
    Co-authored-by: Harshit Singh Lodha <hsinghlodha@newrelic.com>

---
+++ instrumentation-security/jdbc-jtds-generic/src/main/java/net/sourceforge/jtds/jdbc/Driver_Instrumentation.java
@@ -0,0 +1,43 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package net.sourceforge.jtds.jdbc;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.security.java.jdbc.jtds.JdbcHelper;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Weave(type = MatchType.BaseClass, originalName = "net.sourceforge.jtds.jdbc.Driver")
+public class Driver_Instrumentation {
+
+    private void postHookProcessing(Connection connection) {
+        try {
+            String vendor;
+            if(NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
+                vendor = NewRelicSecurity.getAgent().getSecurityMetaData().getCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, String.class);
+                if(vendor == null || vendor.trim().isEmpty()){
+                    vendor = JdbcHelper.detectDatabaseProduct(connection.getMetaData().getDatabaseProductName());
+                    NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, vendor);
+                }
+            }
+        } catch (Exception ignored) {}
+    }
+
+    public Connection connect(String url, Properties props) throws SQLException {
+        Connection connection = Weaver.callOriginal();
+        postHookProcessing(connection);
+        return connection;
+    }
+
+}
---
+++ instrumentation-security/jdbc-jtds-generic/src/main/java/net/sourceforge/jtds/jdbcx/JtdsDataSource_Instrumentation.java
@@ -0,0 +1,48 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package net.sourceforge.jtds.jdbcx;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.security.java.jdbc.jtds.JdbcHelper;
+
+import java.sql.Connection;
+
+@Weave(type = MatchType.BaseClass, originalName = "net.sourceforge.jtds.jdbcx.JtdsDataSource")
+public abstract class JtdsDataSource_Instrumentation {
+
+    private void postHookProcessing(Connection connection) {
+        try {
+            String vendor;
+            if(NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
+                vendor = NewRelicSecurity.getAgent().getSecurityMetaData().getCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, String.class);
+                if(vendor == null || vendor.trim().isEmpty()){
+                    vendor = JdbcHelper.detectDatabaseProduct(connection.getMetaData().getDatabaseProductName());
+                    NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, vendor);
+                }
+            }
+        } catch (Exception ignored) {}
+    }
+
+    public Connection getConnection() throws Exception {
+        Connection connection = Weaver.callOriginal();
+        postHookProcessing(connection);
+        return connection;
+    }
+
+    // This is a tracer because it's common for these methods to delegate to each other and we don't want double counts
+
+    public Connection getConnection(String username, String password) throws Exception {
+        Connection connection = Weaver.callOriginal();
+        postHookProcessing(connection);
+        return connection;
+    }
+}
