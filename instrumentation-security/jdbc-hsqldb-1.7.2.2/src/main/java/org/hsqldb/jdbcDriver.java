1c34662459d0e54026751fee28782af9118e85da
lovesh-ap <lbaya@newrelic.com>
2022-12-28 10:16:31 +0530

    Instrumentation adopt for hsqldb
    Co-authored-by: Harshit Singh Lodha <hsinghlodha@newrelic.com>

---
+++ instrumentation-security/jdbc-hsqldb-1.7.2.2/src/main/java/org/hsqldb/jdbcDriver.java
@@ -0,0 +1,29 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.hsqldb;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Weave
+public abstract class jdbcDriver {
+
+    public Connection connect(String url, Properties props) throws SQLException {
+        if(NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
+            NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, JDBCVendor.HSQLDB);
+        }
+        return Weaver.callOriginal();
+    }
+
+}
