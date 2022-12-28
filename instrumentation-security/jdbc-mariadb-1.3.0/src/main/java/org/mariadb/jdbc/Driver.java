ba6706dbf6cdd4707ff63948b027a52e2139bbc7
lovesh-ap <lbaya@newrelic.com>
2022-12-28 10:34:40 +0530

    Instrumentation adopt for mariadb
    Co-authored-by: Harshit Singh Lodha <hsinghlodha@newrelic.com>

---
+++ instrumentation-security/jdbc-mariadb-1.3.0/src/main/java/org/mariadb/jdbc/Driver.java
@@ -0,0 +1,28 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.mariadb.jdbc;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+@Weave
+public abstract class Driver {
+
+    public Connection connect(String url, Properties props) {
+        if(NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
+            NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, JDBCVendor.MARIA_DB);
+        }
+        return Weaver.callOriginal();
+    }
+
+}
