a2de152a6d774872752ab5f311519024c36de720
lovesh-ap <lbaya@newrelic.com>
2022-12-28 17:08:18 +0530

    Adopt instrumentation of jdbc mysql 3.x, 6.x and 8.x
    Co-authored-by: Harshit Singh Lodha <hsinghlodha@newrelic.com>

---
+++ instrumentation-security/jdbc-mysql-8.0.11/src/main/java/com/mysql/cj/jdbc/NonRegisteringDriver.java
@@ -0,0 +1,29 @@
+/*
+ *
+ *  * Copyright 2020 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.mysql.cj.jdbc;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Weave(type = MatchType.BaseClass)
+public abstract class NonRegisteringDriver {
+
+    public java.sql.Connection connect(String url, Properties props) throws SQLException {
+        if(NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
+            NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, JDBCVendor.MYSQL);
+        }
+        return Weaver.callOriginal();
+    }
+
+}
