329d576b9fb0beada1bfc0de9ea24acd036399c1
Harshit Singh Lodha <hsinghlodha@newrelic.com>
2022-12-29 17:26:22 +0530

    Changes for Apache Http Client 4.x inst module.

--- instrumentation-security/httpclient-4.0/src/main/java/com/nr/agent/instrumentation/security/httpclient40/SecurityHelper.java
+++ instrumentation-security/httpclient-4.0/src/main/java/com/nr/agent/instrumentation/security/httpclient40/SecurityHelper.java
@@ -1,8 +1,10 @@
-package com.nr.agent.instrumentation.security.httpclient30;
+package com.nr.agent.instrumentation.security.httpclient40;
 
 public class SecurityHelper {
+
     public static final String METHOD_NAME_EXECUTE = "execute";
     public static final String NULL_STRING = "null";
+
     public static String getURI(String scheme, String host, int port, String path) {
         StringBuilder sb = new StringBuilder();
         if (scheme != null) {
