92bd4bbc7b640d7fe9d6ad8e6b6e36870cda6a1e
Monu Lakshkar <monu@k2io.com>
2023-01-09 16:30:53 +0530

    Renamed classes of jdbc-postgresql-9.4.1208 module

--- instrumentation-security/jdbc-postgresql-9.4.1208/src/main/java/org/postgresql/ds/common/BaseDataSource_Instrumentation.java
+++ instrumentation-security/jdbc-postgresql-9.4.1208/src/main/java/org/postgresql/ds/common/BaseDataSource_Instrumentation.java
@@ -5,25 +5,23 @@
  *
  */
 
-package org.postgresql;
+package org.postgresql.ds.common;
 
 import com.newrelic.api.agent.security.NewRelicSecurity;
 import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Properties;
 
-@Weave
-public abstract class Driver {
+@Weave(type = MatchType.BaseClass, originalName = "org.postgresql.ds.common.BaseDataSource")
+public abstract class BaseDataSource_Instrumentation {
 
-    public Connection connect(String url, Properties props) throws SQLException {
+    public Connection getConnection(String userID, String pass) throws Exception {
         if(NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
             NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, JDBCVendor.POSTGRES);
         }
         return Weaver.callOriginal();
     }
-
 }
