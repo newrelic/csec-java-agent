18891d84c9fd9c0095bcd87d638abbd6848af1db
Monu Lakshkar <monu@k2io.com>
2023-01-11 13:11:44 +0530

    Added more test cases for `Driver` class using JdbcConnection class
    Added more test cases for `Driver` class using JdbcConnection class

--- instrumentation-security/jdbc-h2/src/test/java/com/nr/instrumentation/org/h2/H2Test.java
+++ instrumentation-security/jdbc-h2/src/test/java/com/nr/instrumentation/org/h2/H2Test.java
@@ -5,9 +5,12 @@ import com.newrelic.agent.security.introspec.SecurityInstrumentationTestRunner;
 import com.newrelic.agent.security.introspec.SecurityIntrospector;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.security.schema.JDBCVendor;
+import org.h2.jdbc.JdbcConnection;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -19,6 +22,7 @@ import java.util.Properties;
  */
 @RunWith(SecurityInstrumentationTestRunner.class)
 @InstrumentationTestConfig(includePrefixes = { "org.h2", "java.sql" })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class H2Test {
     private static final String DB_DRIVER = "org.h2.Driver";
     private static final String DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
@@ -38,6 +42,16 @@ public class H2Test {
     @Test
     public void testConnect1() throws SQLException {
         getConnection1();
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", vendor, JDBCVendor.H2);
+
+    }
+
+    @Test
+    public void testConnect2() throws SQLException {
+        getConnection2();
 
         SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
         String vendor = introspector.getJDBCVendor();
@@ -68,7 +82,26 @@ public class H2Test {
             Properties info = new Properties();
             info.put("user", DB_USER);
             info.put("password", DB_PASSWORD);
+            c= new JdbcConnection(DB_CONNECTION, info);
             c = DriverManager.getConnection(DB_CONNECTION, info);
+        } catch (Exception e) {
+            System.out.println("Error in DB connection: " + e);
+        } finally {
+            if (c != null) {
+                c.close();
+            }
+        }
+    }
+
+    @Trace(dispatcher = true)
+    private void getConnection2() throws SQLException {
+        Connection c = null;
+        try {
+            Class.forName(DB_DRIVER);
+            Properties info = new Properties();
+            info.put("user", DB_USER);
+            info.put("password", DB_PASSWORD);
+            c = new JdbcConnection(DB_CONNECTION, info);
         } catch (Exception e) {
             System.out.println("Error in DB connection: " + e);
         } finally {
