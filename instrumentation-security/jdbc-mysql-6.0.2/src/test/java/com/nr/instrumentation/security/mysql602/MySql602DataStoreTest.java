0573176173a4b5c000f36a68abf125cc8b45a0ea
Monu Lakshkar <monu@k2io.com>
2023-01-12 15:56:45 +0530

    Added more test cases using different `DataSource` classes for 6.0.2 and 8.0.11

--- instrumentation-security/jdbc-mysql-6.0.2/src/test/java/com/nr/instrumentation/security/mysql602/MySql602DataStoreTest.java
+++ instrumentation-security/jdbc-mysql-6.0.2/src/test/java/com/nr/instrumentation/security/mysql602/MySql602DataStoreTest.java
@@ -8,10 +8,15 @@ package com.nr.instrumentation.security.mysql602;
 
 import ch.vorburger.mariadb4j.DB;
 import ch.vorburger.mariadb4j.DBConfigurationBuilder;
+import com.mysql.cj.fabric.jdbc.FabricMySQLDataSource;
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
 import com.mysql.cj.jdbc.MysqlDataSource;
+import com.mysql.cj.jdbc.MysqlXADataSource;
+import com.newrelic.agent.deps.org.jetbrains.annotations.NotNull;
 import com.newrelic.agent.security.introspec.InstrumentationTestConfig;
 import com.newrelic.agent.security.introspec.SecurityInstrumentationTestRunner;
 import com.newrelic.agent.security.introspec.SecurityIntrospector;
+import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.security.schema.JDBCVendor;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -23,32 +28,29 @@ import org.junit.runners.MethodSorters;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 @RunWith(SecurityInstrumentationTestRunner.class)
 @InstrumentationTestConfig(includePrefixes = {"com.mysql.cj"})
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MySql602DataStoreTest {
-
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+    private static String DB_CONNECTION;
+    private static String DB_NAME;
     private static DB mariaDb;
-    private static String connectionString;
-    private static String dbName;
-    private static List<String> QUERIES = new ArrayList<>();
 
     @BeforeClass
     public static void setUpDb() throws Exception {
-        QUERIES.add("select * from testQuery");
         DBConfigurationBuilder builder = DBConfigurationBuilder.newBuilder()
                 .setPort(0); // This will automatically find a free port
 
-        dbName = "MariaDB" + System.currentTimeMillis();
+        DB_NAME = "MariaDB" + System.currentTimeMillis();
         mariaDb = DB.newEmbeddedDB(builder.build());
-        connectionString = builder.getURL(dbName);
+        DB_CONNECTION = builder.getURL(DB_NAME) + "?useSSL=false";
         mariaDb.start();
 
-        mariaDb.createDB(dbName);
-        mariaDb.source("maria-db-test.sql", null, null, dbName);
+        mariaDb.createDB(DB_NAME);
+        mariaDb.source("maria-db-test.sql", null, null, DB_NAME);
     }
 
     @AfterClass
@@ -57,25 +59,180 @@ public class MySql602DataStoreTest {
     }
 
     @Test
-    public void testGetConnection() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        MysqlDataSource mysqlDataSource = new MysqlDataSource();
-        mysqlDataSource.setUrl(connectionString);
-        mysqlDataSource.setUser("root");
-        mysqlDataSource.setPassword("");
-        Connection conn = null;
-
+    public void testGetConnectionMysqlDataSource() {
         try {
-            conn = mysqlDataSource.getConnection();
-        }
-        finally {
-            if (conn!=null) {
-                conn.close();
-            }
+            callGetConnectionMysqlDataSource();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection");
         }
 
         SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
         String vendor = introspector.getJDBCVendor();
-        Assert.assertEquals("Incorrect DB vendor", vendor, JDBCVendor.MYSQL);
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testGetConnectionMysqlDataSource1() {
+        try {
+            callGetConnectionMysqlDataSource1();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection");
+        }
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testGetConnectionFabricMySQLDataSource() {
+        try {
+            callGetConnectionFabricMySQLDataSource();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection");
+        }
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testGetConnectionFabricMySQLDataSource1() {
+        try {
+            callGetConnectionFabricMySQLDataSource1();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection");
+        }
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testGetConnectionMysqlConnectionPoolDataSource() {
+        try {
+            callGetConnectionMysqlConnectionPoolDataSource();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection");
+        }
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testGetConnectionMysqlConnectionPoolDataSource1() {
+        try {
+            callGetConnectionMysqlConnectionPoolDataSource1();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection");
+        }
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testGetConnectionMysqlXADataSource() {
+        try {
+            callGetConnectionMysqlXADataSource();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection");
+        }
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testGetConnectionMysqlXADataSource1() {
+        try {
+            callGetConnectionMysqlXADataSource1();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection");
+        }
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Trace(dispatcher = true)
+    private void callGetConnectionMysqlDataSource() throws SQLException {
+        getConnection(new MysqlDataSource());
+    }
+
+    @Trace(dispatcher = true)
+    private void callGetConnectionMysqlDataSource1() throws SQLException {
+        getConnection1(new MysqlDataSource());
+    }
+
+    @Trace(dispatcher = true)
+    private void callGetConnectionFabricMySQLDataSource() throws SQLException {
+        getConnection(new FabricMySQLDataSource());
+    }
+
+    @Trace(dispatcher = true)
+    private void callGetConnectionFabricMySQLDataSource1() throws SQLException {
+        getConnection1(new FabricMySQLDataSource());
+    }
+
+    @Trace(dispatcher = true)
+    private void callGetConnectionMysqlConnectionPoolDataSource() throws SQLException {
+        getConnection(new MysqlConnectionPoolDataSource());
+    }
+
+    @Trace(dispatcher = true)
+    private void callGetConnectionMysqlConnectionPoolDataSource1() throws SQLException {
+        getConnection1(new MysqlConnectionPoolDataSource());
+    }
+
+    @Trace(dispatcher = true)
+    private void callGetConnectionMysqlXADataSource() throws SQLException {
+        getConnection(new MysqlXADataSource());
+    }
+
+    @Trace(dispatcher = true)
+    private void callGetConnectionMysqlXADataSource1() throws SQLException {
+        getConnection1(new MysqlXADataSource());
+    }
+
+    private void getConnection(@NotNull MysqlDataSource baseDataSource) throws SQLException {
+        baseDataSource.setURL(DB_CONNECTION);
+        baseDataSource.setDatabaseName(DB_NAME);
+        Connection conn = null;
+
+        try {
+            conn = baseDataSource.getConnection(DB_USER, DB_PASSWORD);
+        } catch (Exception e) {
+            System.out.println("Error in DB connection "+e);
+        } finally {
+            if (conn!=null) {
+                conn.close();
+            }
+        }
+    }
+
+    private void getConnection1(@NotNull MysqlDataSource baseDataSource) throws SQLException {
+        baseDataSource.setURL(DB_CONNECTION);
+        baseDataSource.setDatabaseName(DB_NAME);
+        baseDataSource.setUser(DB_USER);
+        baseDataSource.setPassword(DB_PASSWORD);
+        Connection conn = null;
+
+        try {
+            conn = baseDataSource.getConnection();
+        } catch (Exception e) {
+            System.out.println("Error in DB connection "+e);
+        } finally {
+            if (conn!=null) {
+                conn.close();
+            }
+        }
     }
 }
--- instrumentation-security/jdbc-mysql-6.0.2/src/test/java/com/nr/instrumentation/security/mysql602/MySql602DriverTest.java
+++ instrumentation-security/jdbc-mysql-6.0.2/src/test/java/com/nr/instrumentation/security/mysql602/MySql602DriverTest.java
@@ -8,10 +8,10 @@ package com.nr.instrumentation.security.mysql602;
 
 import ch.vorburger.mariadb4j.DB;
 import ch.vorburger.mariadb4j.DBConfigurationBuilder;
-import com.mysql.cj.api.jdbc.JdbcConnection;
 import com.newrelic.agent.security.introspec.InstrumentationTestConfig;
 import com.newrelic.agent.security.introspec.SecurityInstrumentationTestRunner;
 import com.newrelic.agent.security.introspec.SecurityIntrospector;
+import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.security.schema.JDBCVendor;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -24,38 +24,30 @@ import org.junit.runners.MethodSorters;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Properties;
 
 @RunWith(SecurityInstrumentationTestRunner.class)
 @InstrumentationTestConfig(includePrefixes = {"com.mysql.cj"})
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MySql602DriverTest {
-
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+    private static String DB_CONNECTION;
+    private static String DB_NAME;
     private static DB mariaDb;
-
-    private static String connectionString;
-    private static String dbName;
-
-    private static JdbcConnection connection;
-
-    private static List<String> QUERIES = new ArrayList<>();
 
     @BeforeClass
     public static void setUpDb() throws Exception {
-        QUERIES.add("select * from testQuery");
         DBConfigurationBuilder builder = DBConfigurationBuilder.newBuilder()
                 .setPort(0); // This will automatically find a free port
 
-        dbName = "MariaDB" + System.currentTimeMillis();
+        DB_NAME = "MariaDB" + System.currentTimeMillis();
         mariaDb = DB.newEmbeddedDB(builder.build());
-        connectionString = builder.getURL(dbName);
+        DB_CONNECTION = builder.getURL(DB_NAME) + "?useSSL=false";
         mariaDb.start();
 
-        mariaDb.createDB(dbName);
-        mariaDb.source("maria-db-test.sql", null, null, dbName);
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = (JdbcConnection) DriverManager.getConnection(connectionString, "root", "");
+        mariaDb.createDB(DB_NAME);
+        mariaDb.source("maria-db-test.sql", null, null, DB_NAME);
     }
 
     @AfterClass
@@ -65,17 +57,82 @@ public class MySql602DriverTest {
 
     @Test
     public void testConnect() throws SQLException {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(connectionString, "root", "");
-        }
-        finally {
-            if (conn!=null) {
-                conn.close();
-            }
-        }
+        getConnection();
+
         SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
         String vendor = introspector.getJDBCVendor();
-        Assert.assertEquals("Incorrect DB vendor", vendor, JDBCVendor.MYSQL);
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testConnect1() throws SQLException {
+        getConnection1();
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Test
+    public void testConnect2() throws SQLException {
+        getConnection2();
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MYSQL, vendor);
+    }
+
+    @Trace(dispatcher = true)
+    private void getConnection() throws SQLException {
+        Connection dbConnection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (dbConnection!=null) {
+                dbConnection.close();
+            }
+        }
+    }
+
+    @Trace(dispatcher = true)
+    private void getConnection1() throws SQLException {
+        Connection dbConnection = null;
+
+        try {
+            Properties info = new Properties();
+            info.put("user", DB_USER);
+            info.put("password", DB_PASSWORD);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, info);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (dbConnection!=null) {
+                dbConnection.close();
+            }
+        }
+    }
+
+    @Trace(dispatcher = true)
+    private void getConnection2() throws SQLException {
+        Connection dbConnection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            dbConnection = DriverManager.getConnection(DB_CONNECTION);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (dbConnection!=null) {
+                dbConnection.close();
+            }
+        }
     }
 }
