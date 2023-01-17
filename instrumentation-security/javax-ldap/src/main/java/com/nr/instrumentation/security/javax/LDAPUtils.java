ac425fe7c0c9bb4d5f995ed2c8c6333a7cc98220
lovesh-ap <lbaya@newrelic.com>
2023-01-17 14:52:05 +0530

    Adopt instrumentation of javax LDAP
    Co-authored-by: Harshit Singh Lodha <hsinghlodha@newrelic.com>

---
+++ instrumentation-security/javax-ldap/src/main/java/com/nr/instrumentation/security/javax/LDAPUtils.java
@@ -0,0 +1,7 @@
+package com.nr.instrumentation.security.javax;
+
+public class LDAPUtils {
+
+    public static final String NR_SEC_CUSTOM_ATTRIB_NAME = "LDAP_OPERATION_LOCK-";
+    public static final String METHOD_SEARCH = "search";
+}
---
+++ instrumentation-security/javax-ldap/src/main/java/javax/naming/directory/DirContext_Instrumentation.java
@@ -0,0 +1,112 @@
+package javax.naming.directory;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.instrumentation.helpers.GenericHelper;
+import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.StringUtils;
+import com.newrelic.api.agent.security.schema.exceptions.NewRelicSecurityException;
+import com.newrelic.api.agent.security.schema.operation.LDAPOperation;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.security.javax.LDAPUtils;
+
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import java.sql.ResultSet;
+
+@Weave(type = MatchType.Interface, originalName = "javax.naming.directory.DirContext")
+public abstract class DirContext_Instrumentation implements Context {
+
+    public NamingEnumeration<SearchResult>
+    search(Name name,
+           String filterExpr,
+           Object[] filterArgs,
+           SearchControls cons)
+            throws NamingException {
+        boolean isLockAcquired = acquireLockIfPossible();
+        AbstractOperation operation = null;
+        if(isLockAcquired) {
+            operation = preprocessSecurityHook(name.toString(), filterExpr, LDAPUtils.METHOD_SEARCH);
+        }
+
+        NamingEnumeration<SearchResult> returnVal = null;
+        try {
+            returnVal = Weaver.callOriginal();
+        } finally {
+            if(isLockAcquired){
+                releaseLock();
+            }
+        }
+        registerExitOperation(isLockAcquired, operation);
+        return returnVal;
+    }
+
+    public NamingEnumeration<SearchResult>
+    search(String name,
+           String filterExpr,
+           Object[] filterArgs,
+           SearchControls cons)
+            throws NamingException {
+        boolean isLockAcquired = acquireLockIfPossible();
+        AbstractOperation operation = null;
+        if(isLockAcquired) {
+            operation = preprocessSecurityHook(name.toString(), filterExpr, LDAPUtils.METHOD_SEARCH);
+        }
+
+        NamingEnumeration<SearchResult> returnVal = null;
+        try {
+            returnVal = Weaver.callOriginal();
+        } finally {
+            if(isLockAcquired){
+                releaseLock();
+            }
+        }
+        registerExitOperation(isLockAcquired, operation);
+        return returnVal;
+    }
+
+    private void registerExitOperation(boolean isProcessingAllowed, AbstractOperation operation) {
+        try {
+            if (operation == null || !isProcessingAllowed || !NewRelicSecurity.isHookProcessingActive() ||
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty() || GenericHelper.skipExistsEvent()
+            ) {
+                return;
+            }
+            NewRelicSecurity.getAgent().registerExitEvent(operation);
+        } catch (Throwable ignored){}
+    }
+
+    private AbstractOperation preprocessSecurityHook (String name, String filter, String methodName){
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive() ||
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty() ||
+                    StringUtils.isAnyBlank(name, filter)){
+                return null;
+            }
+            LDAPOperation ldapOperation = new LDAPOperation(name, filter, this.getClass().getName(), methodName);
+            return ldapOperation;
+        } catch (Throwable e) {
+            if (e instanceof NewRelicSecurityException) {
+                throw e;
+            }
+        }
+        return null;
+    }
+
+    private void releaseLock() {
+        try {
+            GenericHelper.releaseLock(LDAPUtils.NR_SEC_CUSTOM_ATTRIB_NAME);
+        } catch (Throwable ignored) {}
+    }
+
+    private boolean acquireLockIfPossible() {
+        try {
+            return GenericHelper.acquireLockIfPossible(LDAPUtils.NR_SEC_CUSTOM_ATTRIB_NAME);
+        } catch (Throwable ignored) {}
+        return false;
+    }
+
+}
