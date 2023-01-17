3b5edf5e2c6a64818615e16b46de048b6841aad2
lovesh-ap <lbaya@newrelic.com>
2023-01-17 14:53:46 +0530

    Adopt instrumentation of apache camel XPATH lib
    Co-authored-by: Harshit Singh Lodha <hsinghlodha@newrelic.com>

---
+++ instrumentation-security/camel-xpath/src/main/java/org/apache/camel/builder/BuilderSupport_Instrumentation.java
@@ -0,0 +1,77 @@
+package org.apache.camel.builder;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.instrumentation.helpers.GenericHelper;
+import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.StringUtils;
+import com.newrelic.api.agent.security.schema.exceptions.NewRelicSecurityException;
+import com.newrelic.api.agent.security.schema.operation.XPathOperation;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.security.xpath.camel.XPATHUtils;
+import org.apache.camel.support.builder.Namespaces;
+
+@Weave(type = MatchType.BaseClass, originalName = "org.apache.camel.builder.BuilderSupport")
+public class BuilderSupport_Instrumentation {
+
+    private void registerExitOperation(boolean isProcessingAllowed, AbstractOperation operation) {
+        try {
+            if (operation == null || !isProcessingAllowed || !NewRelicSecurity.isHookProcessingActive() ||
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty() || GenericHelper.skipExistsEvent()
+            ) {
+                return;
+            }
+            NewRelicSecurity.getAgent().registerExitEvent(operation);
+        } catch (Throwable ignored){}
+    }
+
+    private AbstractOperation preprocessSecurityHook (String expression, String methodName){
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive() ||
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty() ||
+                    StringUtils.isBlank(expression)){
+                return null;
+            }
+            XPathOperation xPathOperation = new XPathOperation(expression, this.getClass().getName(), methodName);
+            return xPathOperation;
+        } catch (Throwable e) {
+            if (e instanceof NewRelicSecurityException) {
+                throw e;
+            }
+        }
+        return null;
+    }
+
+    private void releaseLock() {
+        try {
+            GenericHelper.releaseLock(XPATHUtils.NR_SEC_CUSTOM_ATTRIB_NAME);
+        } catch (Throwable ignored) {}
+    }
+
+    private boolean acquireLockIfPossible() {
+        try {
+            return GenericHelper.acquireLockIfPossible(XPATHUtils.NR_SEC_CUSTOM_ATTRIB_NAME);
+        } catch (Throwable ignored) {}
+        return false;
+    }
+
+    public ValueBuilder xpath(String value, Class<?> resultType, Namespaces namespaces) {
+        boolean isLockAcquired = acquireLockIfPossible();
+        AbstractOperation operation = null;
+        if(isLockAcquired) {
+            operation = preprocessSecurityHook(value, XPATHUtils.METHOD_XPATH);
+        }
+
+        ValueBuilder returnVal = null;
+        try {
+            returnVal = Weaver.callOriginal();
+        } finally {
+            if(isLockAcquired){
+                releaseLock();
+            }
+        }
+        registerExitOperation(isLockAcquired, operation);
+        return returnVal;
+    }
+}
