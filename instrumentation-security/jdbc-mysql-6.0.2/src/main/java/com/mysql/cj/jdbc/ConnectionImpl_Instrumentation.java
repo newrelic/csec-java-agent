a874d2f6be48ecdd45d5e063b5d3eae1ca3bf6f2
Monu Lakshkar <monu@k2io.com>
2023-02-10 13:19:00 +0530

    Added hooks for ConnectionImpl [mysql 6.0.2 & 8.0.11]

---
+++ instrumentation-security/jdbc-mysql-6.0.2/src/main/java/com/mysql/cj/jdbc/ConnectionImpl_Instrumentation.java
@@ -0,0 +1,19 @@
+package com.mysql.cj.jdbc;
+
+import com.mysql.cj.core.ConnectionString;
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Weave(type = MatchType.ExactClass, originalName = "com.mysql.cj.jdbc.ConnectionImpl")
+public class ConnectionImpl_Instrumentation {
+    public ConnectionImpl_Instrumentation(ConnectionString connectionString, String hostToConnectTo, int portToConnectTo, Properties info) throws SQLException {
+        if (NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
+            NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, JDBCVendor.MYSQL);
+        }
+    }
+}
