588f6f4f35abe3c6827c220b45f91d0c730d8b0d
Monu Lakshkar <monu@k2io.com>
2023-02-10 13:27:07 +0530

    Added hooks for MySQLConnection and MariaDbConnection

---
+++ instrumentation-security/jdbc-mariadb-1.1.7/src/main/java/org/mariadb/jdbc/MySQLConnection_Instrumentation.java
@@ -0,0 +1,17 @@
+package org.mariadb.jdbc;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.WeaveAllConstructors;
+
+@Weave(type = MatchType.ExactClass, originalName = "org.mariadb.jdbc.MySQLConnection")
+public class MySQLConnection_Instrumentation {
+    @WeaveAllConstructors
+    private MySQLConnection_Instrumentation() {
+        if (NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
+            NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(JDBCVendor.META_CONST_JDBC_VENDOR, JDBCVendor.MARIA_DB);
+        }
+    }
+}
