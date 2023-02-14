42cf7c48ecf2a8079c1fac653923ba26d4366941
idawda <idawda@newrelic.com>
2023-02-14 10:56:17 +0530

    Added okhttp-3.5 unit test

---
+++ instrumentation-security/okhttp-3.5.0/src/test/java/com/nr/instrumentation/security/okhttp30/RealCallTest.java
@@ -0,0 +1,60 @@
+package com.nr.instrumentation.security.okhttp30;
+
+import com.newrelic.agent.security.introspec.HttpTestServer;
+import com.newrelic.agent.security.introspec.InstrumentationTestConfig;
+import com.newrelic.agent.security.introspec.SecurityInstrumentationTestRunner;
+import com.newrelic.agent.security.introspec.SecurityIntrospector;
+import com.newrelic.agent.security.introspec.internal.HttpServerRule;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.VulnerabilityCaseType;
+import com.newrelic.api.agent.security.schema.operation.SSRFOperation;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.List;
+
+@RunWith(SecurityInstrumentationTestRunner.class)
+@InstrumentationTestConfig(includePrefixes = { "com.nr.agent.instrumentation.security.okhttp35" })
+public class RealCallTest {
+    @ClassRule
+    public static HttpServerRule server = new HttpServerRule();
+
+    @Test
+    public void testExecute() throws Exception {
+        try {
+            httpClientExternal(server.getEndPoint().toString());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        List<AbstractOperation> operations = introspector.getOperations();
+        Assert.assertTrue("No operations detected", operations.size() > 0);
+
+        SSRFOperation operation = (SSRFOperation) operations.get(0);
+        Assert.assertEquals("Invalid executed parameters.", server.getEndPoint().toString(), operation.getArg());
+        Assert.assertEquals("Invalid event category.", VulnerabilityCaseType.HTTP_REQUEST, operation.getCaseType());
+        Assert.assertEquals("Invalid executed method name.", "execute", operation.getMethodName());
+    }
+
+    @Trace(dispatcher = true)
+    private void httpClientExternal(String host) throws IOException {
+        final OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder()
+                .url(host)
+                .addHeader(HttpTestServer.DO_CAT, String.valueOf(true))
+                .build();
+
+        Response response = client.newCall(request).execute();
+        response.body().close();
+    }
+}
