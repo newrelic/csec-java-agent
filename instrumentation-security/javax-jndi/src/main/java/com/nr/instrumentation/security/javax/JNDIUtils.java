9321b8291b5514a3b0bd108b89865d029bc04cc3
lovesh-ap <lbaya@newrelic.com>
2023-02-24 09:52:22 +0530

    [INSTRUMENTATION] Adopt NR instrumnetation for Javax JNDI
    Co-authored-by: Harshit Singh Lodha <hsinghlodha@newrelic.com>

---
+++ instrumentation-security/javax-jndi/src/main/java/com/nr/instrumentation/security/javax/JNDIUtils.java
@@ -0,0 +1,44 @@
+package com.nr.instrumentation.security.javax;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.StringUtils;
+import com.newrelic.api.agent.security.schema.operation.FileOperation;
+import com.newrelic.api.agent.security.schema.operation.SSRFOperation;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class JNDIUtils {
+
+    public static final String NR_SEC_CUSTOM_ATTRIB_NAME = "JNDI_OPERATION_LOCK-";
+    public static final String METHOD_LOOKUP = "lookup";
+
+    public static List<AbstractOperation> handleJNDIHook(Enumeration<String> names, String methodName, String className) {
+        List<AbstractOperation> abstractOperations = new ArrayList<>();
+        while (names.hasMoreElements()) {
+            abstractOperations.add(handleJNDIHook(names.nextElement(), methodName, className));
+        }
+        return abstractOperations;
+    }
+
+    public static AbstractOperation handleJNDIHook(String name, String methodName, String className) {
+        try {
+            URI url = new URI(name);
+            if (StringUtils.isNotBlank(url.getScheme()) &&
+                    StringUtils.equalsAny(url.getScheme().toLowerCase(), "ldap", "rmi", "dns", "iiop")) {
+                SSRFOperation operation = new SSRFOperation(name, className, methodName);
+                NewRelicSecurity.getAgent().registerOperation(operation);
+                return operation;
+            } else {
+                FileOperation operation = new FileOperation(name, className, methodName, false);
+                NewRelicSecurity.getAgent().registerOperation(operation);
+                return operation;
+            }
+        } catch (Exception ignored) {}
+        return null;
+    }
+
+}
