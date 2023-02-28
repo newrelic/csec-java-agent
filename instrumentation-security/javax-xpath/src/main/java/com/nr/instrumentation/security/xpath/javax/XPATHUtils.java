6bf4d76bd4f0297b8cf41831601fc63be13b29df
lovesh-ap <lbaya@newrelic.com>
2023-02-28 19:04:54 +0530

    [INSTRUMENTATION] adopt instrumentation for Javax XPATH lib
    Co-authored-by: Harshit Singh Lodha <hsinghlodha@newrelic.com>

---
+++ instrumentation-security/javax-xpath/src/main/java/com/nr/instrumentation/security/xpath/javax/XPATHUtils.java
@@ -0,0 +1,7 @@
+package com.nr.instrumentation.security.xpath.javax;
+
+public class XPATHUtils {
+
+    public static final String NR_SEC_CUSTOM_ATTRIB_NAME = "XPATH_OPERATION_LOCK_JAVAXPATH-";
+    public static final String METHOD_EVALUATE = "evaluate";
+}
