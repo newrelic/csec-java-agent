031bbd160e6d93938ecae7e49a762849d65334bf
Monu Lakshkar <monu@k2io.com>
2023-03-01 19:16:12 +0530

    Instrumentation of r2dbc-h2

---
+++ instrumentation-security/r2dbc-h2/src/main/java/io/r2dbc/h2/client/Client_Instrumentation.java
@@ -0,0 +1,87 @@
+package io.r2dbc.h2.client;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.instrumentation.helpers.JdbcHelper;
+import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.R2DBCVendor;
+import com.newrelic.api.agent.security.schema.exceptions.NewRelicSecurityException;
+import com.newrelic.api.agent.security.schema.operation.SQLOperation;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import io.r2dbc.spi.Result;
+import org.reactivestreams.Publisher;
+
+@Weave(type = MatchType.Interface, originalName = "io.r2dbc.h2.client.Client")
+public class Client_Instrumentation {
+
+    public void execute(String sql) {
+        boolean isLockAcquired = acquireLockIfPossible();
+        AbstractOperation operation = null;
+        if (isLockAcquired) {
+            operation = preprocessSecurityHook(sql, JdbcHelper.METHOD_EXECUTE);
+        }
+        Publisher<? extends Result> returnVal;
+        try {
+            Weaver.callOriginal();
+        } finally {
+            if (isLockAcquired) {
+                releaseLock();
+            }
+        }
+        registerExitOperation(isLockAcquired, operation);
+        return;
+    }
+
+
+    private void registerExitOperation(boolean isProcessingAllowed, AbstractOperation operation) {
+        try {
+            if (operation == null || !isProcessingAllowed || !NewRelicSecurity.isHookProcessingActive() ||
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty() || JdbcHelper.skipExistsEvent()
+            ) {
+                return;
+            }
+            NewRelicSecurity.getAgent().registerExitEvent(operation);
+        } catch (Throwable ignored) {
+        }
+    }
+
+    private AbstractOperation preprocessSecurityHook(String sql, String methodName) {
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive() ||
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty() ||
+                    sql == null || sql.trim().isEmpty()) {
+                return null;
+            }
+            SQLOperation sqlOperation = new SQLOperation(this.getClass().getName(), methodName);
+            sqlOperation.setQuery(sql);
+            sqlOperation.setDbName(NewRelicSecurity.getAgent().getSecurityMetaData().getCustomAttribute(R2DBCVendor.META_CONST_R2DBC_VENDOR, String.class));
+
+            sqlOperation.setPreparedCall(false);
+
+            NewRelicSecurity.getAgent().registerOperation(sqlOperation);
+            return sqlOperation;
+        } catch (Throwable e) {
+            if (e instanceof NewRelicSecurityException) {
+                e.printStackTrace();
+                throw e;
+            }
+        }
+        return null;
+    }
+
+    private void releaseLock() {
+        try {
+            JdbcHelper.releaseLock();
+        } catch (Throwable ignored) {
+        }
+    }
+
+    private boolean acquireLockIfPossible() {
+        try {
+            return JdbcHelper.acquireLockIfPossible();
+        } catch (Throwable ignored) {
+        }
+        return false;
+    }
+}
