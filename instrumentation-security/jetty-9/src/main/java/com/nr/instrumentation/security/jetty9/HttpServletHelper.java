7ef97ae4253e6a08a4d83893ee4f154a545a53df
Harshit Singh Lodha <hsinghlodha@newrelic.com>
2023-03-06 17:31:34 +0530

    Fix for event not getting generated in jetty inst module.
    Co-authored-by: Lovesh Baya <lbaya@newrelic.com>

--- instrumentation-security/jetty-9/src/main/java/com/nr/instrumentation/security/jetty9/HttpServletHelper.java
+++ instrumentation-security/jetty-9/src/main/java/com/nr/instrumentation/security/jetty9/HttpServletHelper.java
@@ -4,9 +4,13 @@ import com.newrelic.api.agent.security.NewRelicSecurity;
 import com.newrelic.api.agent.security.instrumentation.helpers.ServletHelper;
 import com.newrelic.api.agent.security.schema.AgentMetaData;
 import com.newrelic.api.agent.security.schema.HttpRequest;
+import com.newrelic.api.agent.security.schema.SecurityMetaData;
+import com.newrelic.api.agent.security.schema.exceptions.NewRelicSecurityException;
+import com.newrelic.api.agent.security.schema.operation.RXSSOperation;
 import com.newrelic.api.agent.security.schema.policy.AgentPolicy;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Enumeration;
 import java.util.Map;
 
@@ -17,7 +21,9 @@ public class HttpServletHelper {
     private static final String EMPTY = "";
     private static final String K2_TRACING_HEADER = "K2-TRACING-DATA";
     public static final String QUESTION_MARK = "?";
-    public static final String SERVICE_METHOD_NAME = "service";
+    public static final String SERVICE_METHOD_NAME = "handle";
+    public static final String SERVICE_ASYNC_METHOD_NAME = "handleAsync";
+
 
     public static final String NR_SEC_CUSTOM_ATTRIB_NAME = "SERVLET_LOCK-";
 
@@ -109,5 +115,69 @@ public class HttpServletHelper {
 
     private static String getNrSecCustomAttribName() {
         return NR_SEC_CUSTOM_ATTRIB_NAME + Thread.currentThread().getId();
+    }
+
+    public static void preprocessSecurityHook(HttpServletRequest httpServletRequest) {
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive() || httpServletRequest == null) {
+                return;
+            }
+            SecurityMetaData securityMetaData = NewRelicSecurity.getAgent().getSecurityMetaData();
+
+            HttpRequest securityRequest = securityMetaData.getRequest();
+            if (securityRequest.isRequestParsed()) {
+                return;
+            }
+
+            AgentMetaData securityAgentMetaData = securityMetaData.getMetaData();
+
+            securityRequest.setMethod(httpServletRequest.getMethod());
+            securityRequest.setClientIP(httpServletRequest.getRemoteAddr());
+            securityRequest.setServerPort(httpServletRequest.getLocalPort());
+
+            if (securityRequest.getClientIP() != null && !securityRequest.getClientIP().trim().isEmpty()) {
+                securityAgentMetaData.getIps().add(securityRequest.getClientIP());
+                securityRequest.setClientPort(String.valueOf(httpServletRequest.getRemotePort()));
+            }
+
+            HttpServletHelper.processHttpRequestHeader(httpServletRequest, securityRequest);
+
+            securityMetaData.setTracingHeaderValue(HttpServletHelper.getTraceHeader(securityRequest.getHeaders()));
+
+            securityRequest.setProtocol(httpServletRequest.getScheme());
+            securityRequest.setUrl(httpServletRequest.getRequestURI());
+
+            // TODO: Create OutBoundHttp data here : Skipping for now.
+
+            String queryString = httpServletRequest.getQueryString();
+            if (queryString != null && !queryString.trim().isEmpty()) {
+                securityRequest.setUrl(securityRequest.getUrl() + HttpServletHelper.QUESTION_MARK + queryString);
+            }
+            securityRequest.setContentType(httpServletRequest.getContentType());
+
+            securityAgentMetaData.setServiceTrace(Thread.currentThread().getStackTrace());
+            securityRequest.setRequestParsed(true);
+        } catch (Throwable ignored) {
+        }
+    }
+
+    public static void postProcessSecurityHook(HttpServletRequest request, HttpServletResponse response,
+                                               String className, String methodName) {
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive()
+            ) {
+                return;
+            }
+            RXSSOperation rxssOperation = new RXSSOperation(NewRelicSecurity.getAgent().getSecurityMetaData().getRequest(),
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getResponse(),
+                    className, methodName);
+            NewRelicSecurity.getAgent().registerOperation(rxssOperation);
+
+        } catch (Throwable e) {
+            if (e instanceof NewRelicSecurityException) {
+                e.printStackTrace();
+                throw e;
+            }
+        }
     }
 }
