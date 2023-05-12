f7d95eb5bee34a2602e46522dda2bf4d39288a9d
Monu Lakshkar <monu@k2io.com>
2023-05-12 21:49:30 +0530

    removed accidental commit

--- newrelic-security-api/src/main/java/com/newrelic/api/agent/security/schema/operation/BatchSQLOperation.java
+++ newrelic-security-api/src/main/java/com/newrelic/api/agent/security/schema/operation/BatchSQLOperation.java
@@ -27,12 +27,6 @@ public class BatchSQLOperation extends AbstractOperation {
         }
     }
 
-    public void clearOperation() {
-        if(operations != null && !operations.isEmpty()) {
-            this.operations.clear();
-        }
-    }
-
     @Override
     public boolean isEmpty() {
         return this.operations.isEmpty();
