ba2bff54d8ad83d21fe8a9ad3dc718252ab84e27
Monu Lakshkar <monu@k2io.com>
2023-05-24 22:02:32 +0530

    Getting url mappings for apache-struts2

---
+++ instrumentation-security/apache-struts2/src/main/java/com/nr/instrumentation/security/apache/struts2/StrutsHelper.java
@@ -0,0 +1,33 @@
+package com.nr.instrumentation.security.apache.struts2;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.ApplicationURLMapping;
+import com.opensymphony.xwork2.config.RuntimeConfiguration;
+import com.opensymphony.xwork2.config.entities.ActionConfig;
+import java.util.Map;
+
+public class StrutsHelper {
+
+    private static final String SEPARATOR = "/";
+    private static final String WILDCARD = "*";
+    public static void gatherURLMappings(RuntimeConfiguration runtimeConfig) {
+        try {
+
+            Map<String, Map<String, ActionConfig>> namespaces = runtimeConfig.getActionConfigs();
+            for (Map.Entry<String, Map<String, ActionConfig>> namespace : namespaces.entrySet()) {
+
+                String url = namespace.getKey();
+                for (ActionConfig actionConfig : namespace.getValue().values()) {
+                    String mapping;
+                    if(url.endsWith(SEPARATOR)){
+                        mapping = url + actionConfig.getName();
+                    } else {
+                        mapping = url + SEPARATOR + actionConfig.getName();
+                    }
+                    NewRelicSecurity.getAgent().addURLMapping(new ApplicationURLMapping(WILDCARD, mapping));
+                }
+            }
+        } catch (Exception ignored){
+        }
+    }
+}
