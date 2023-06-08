24d4947069e7cbb781047464ca9b6e13dbf7c257
Monu Lakshkar <mlakshkar@newrelic.com>
2023-06-08 12:58:49 +0530

    Added some key string in helper to be used for request/response body data in the metadata

--- newrelic-security-api/src/main/java/com/newrelic/api/agent/security/instrumentation/helpers/GrpcHelper.java
+++ newrelic-security-api/src/main/java/com/newrelic/api/agent/security/instrumentation/helpers/GrpcHelper.java
@@ -4,6 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GrpcHelper {
+    public static final String NR_SEC_GRPC_REQUEST_DATA = "NR_SEC_GRPC_REQUEST_DATA";
+    public static final String NR_SEC_GRPC_RESPONSE_DATA = "NR_SEC_GRPC_RESPONSE_DATA";
     public static String getFormattedIp(String input) {
         Pattern ipv4Regex = Pattern.compile("(\\d{1,3}\\.){3}\\d{1,3}");
         Pattern ipv6Regex = Pattern.compile("([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}");
