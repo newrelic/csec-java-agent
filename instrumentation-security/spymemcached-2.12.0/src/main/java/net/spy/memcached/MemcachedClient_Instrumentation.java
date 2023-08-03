48f3be2bb1da249d8528062320d79bedca220229
Monu Lakshkar <mlakshkar@newrelic.com>
2023-08-03 17:54:00 +0530

    Added instrumentation of Spymemcached

---
+++ instrumentation-security/spymemcached-2.12.0/src/main/java/net/spy/memcached/MemcachedClient_Instrumentation.java
@@ -0,0 +1,100 @@
+package net.spy.memcached;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.instrumentation.helpers.GenericHelper;
+import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.exceptions.NewRelicSecurityException;
+import com.newrelic.api.agent.security.schema.operation.MemcachedOperation;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import net.spy.memcached.internal.OperationFuture;
+import net.spy.memcached.ops.ConcatenationType;
+import net.spy.memcached.ops.Operation;
+import net.spy.memcached.ops.StoreType;
+import net.spy.memcached.transcoders.Transcoder;
+
+@Weave(originalName = "net.spy.memcached.MemcachedClient")
+public class MemcachedClient_Instrumentation {
+    private <T> OperationFuture<Boolean> asyncStore(StoreType storeType,
+                String key, int exp, T value, Transcoder<T> tc) {
+        boolean isLockAcquired = GenericHelper.acquireLockIfPossible(MemcachedHelper.NR_SEC_CUSTOM_ATTRIB_NAME, value.hashCode());
+        AbstractOperation operation = null;
+        if (isLockAcquired) {
+            operation = preprocessSecurityHook(key, value, this.getClass().getName(), "asyncStore");
+        }
+        OperationFuture<Boolean> returnValue = null;
+        try {
+            returnValue = Weaver.callOriginal();
+        } finally {
+            if (isLockAcquired) {
+                GenericHelper.releaseLock(MemcachedHelper.NR_SEC_CUSTOM_ATTRIB_NAME, value.hashCode());
+            }
+        }
+        registerExitOperation(isLockAcquired, operation);
+        return returnValue;
+    }
+
+    private <T> OperationFuture<Boolean> asyncCat(ConcatenationType catType,
+            long cas, String key, T value, Transcoder<T> tc) {
+        boolean isLockAcquired = GenericHelper.acquireLockIfPossible(MemcachedHelper.NR_SEC_CUSTOM_ATTRIB_NAME, value.hashCode());
+        AbstractOperation operation = null;
+        if (isLockAcquired) {
+            operation = preprocessSecurityHook(key, value, this.getClass().getName(), "asyncCat");
+        }
+        OperationFuture<Boolean> returnValue = null;
+        try {
+            returnValue = Weaver.callOriginal();
+        } finally {
+            if (isLockAcquired) {
+                GenericHelper.releaseLock(MemcachedHelper.NR_SEC_CUSTOM_ATTRIB_NAME, value.hashCode());
+            }
+        }
+        registerExitOperation(isLockAcquired, operation);
+        return returnValue;
+    }
+
+    public <T> OperationFuture<CASResponse>
+    asyncCAS(String key, long casId, int exp, T value, Transcoder<T> tc) {
+        boolean isLockAcquired = GenericHelper.acquireLockIfPossible(MemcachedHelper.NR_SEC_CUSTOM_ATTRIB_NAME, value.hashCode());
+        AbstractOperation operation = null;
+        if (isLockAcquired) {
+            operation = preprocessSecurityHook(key, value, this.getClass().getName(), "asyncCAS");
+        }
+        OperationFuture<CASResponse> returnValue = null;
+        try {
+            returnValue = Weaver.callOriginal();
+        } finally {
+            if (isLockAcquired) {
+                GenericHelper.releaseLock(MemcachedHelper.NR_SEC_CUSTOM_ATTRIB_NAME, value.hashCode());
+            }
+        }
+        registerExitOperation(isLockAcquired, operation);
+        return returnValue;
+    }
+
+    private AbstractOperation preprocessSecurityHook(String key, Object val, String klass, String method) {
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive() || NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()){
+                return null;
+            }
+            MemcachedOperation operation = new MemcachedOperation(key, val, klass, method);
+            NewRelicSecurity.getAgent().registerOperation(operation);
+            return operation;
+        } catch (Throwable e) {
+            if (e instanceof NewRelicSecurityException) {
+                throw e;
+            }
+        }
+        return null;
+    }
+
+    private void registerExitOperation(boolean isProcessingAllowed, AbstractOperation operation) {
+        try {
+            if (operation == null || !isProcessingAllowed || !NewRelicSecurity.isHookProcessingActive() ||
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
+                return;
+            }
+            NewRelicSecurity.getAgent().registerExitEvent(operation);
+        } catch (Throwable ignored){}
+    }
+}
