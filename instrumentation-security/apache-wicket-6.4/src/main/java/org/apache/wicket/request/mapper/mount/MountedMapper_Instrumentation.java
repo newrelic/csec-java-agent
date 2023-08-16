d6016931c910795bcd9eedc0e82ae10fe1dde5a7
idawda <idawda@newrelic.com>
2023-08-16 18:08:21 +0530

    Instrumentation changes for MountedMapper

--- instrumentation-security/apache-wicket-6.4/src/main/java/org/apache/wicket/request/mapper/mount/MountedMapper_Instrumentation.java
+++ instrumentation-security/apache-wicket-6.4/src/main/java/org/apache/wicket/request/mapper/mount/MountedMapper_Instrumentation.java
@@ -2,18 +2,19 @@ package org.apache.wicket.request.mapper.mount;
 
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
-import com.newrelic.api.agent.weaver.WeaveAllConstructors;
-import com.newrelic.api.agent.weaver.Weaver;
 import com.nr.instrumentation.security.apache.wicket6.WicketHelper;
 import org.apache.wicket.request.component.IRequestablePage;
+import org.apache.wicket.request.mapper.parameter.IPageParametersEncoder;
 import org.apache.wicket.util.IProvider;
 
 @Weave(type = MatchType.ExactClass, originalName = "org.apache.wicket.core.request.mapper.MountedMapper")
 public class MountedMapper_Instrumentation {
-    private final String[] mountSegments = Weaver.callOriginal();
-    private final IProvider<Class<? extends IRequestablePage>> pageClassProvider = Weaver.callOriginal();
-    @WeaveAllConstructors
-    public MountedMapper_Instrumentation(){
-        WicketHelper.getMappings(mountSegments, pageClassProvider.get().getName(), false);
+
+    public MountedMapper_Instrumentation(
+            String mountPath,
+            IProvider<Class<? extends IRequestablePage>> pageClassProvider,
+            IPageParametersEncoder pageParametersEncoder
+    ){
+        WicketHelper.getMappings(mountPath, pageClassProvider.get().getName(), false);
     }
 }
