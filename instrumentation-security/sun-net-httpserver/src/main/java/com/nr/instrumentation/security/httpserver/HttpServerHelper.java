9ff10bd37a0f6ec052151105645b8c2f05782ed9
idawda <idawda@newrelic.com>
2023-08-23 14:37:11 +0530

    Added instrumentation for sun-net-httpserver

---
+++ instrumentation-security/sun-net-httpserver/src/main/java/com/nr/instrumentation/security/httpserver/HttpServerHelper.java
@@ -0,0 +1,104 @@
+package com.nr.instrumentation.security.httpserver;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.instrumentation.helpers.ServletHelper;
+import com.newrelic.api.agent.security.schema.AgentMetaData;
+import com.newrelic.api.agent.security.schema.HttpRequest;
+import com.newrelic.api.agent.security.schema.policy.AgentPolicy;
+import com.sun.net.httpserver.Headers;
+
+import java.util.Map;
+
+public class HttpServerHelper {
+    private static final String X_FORWARDED_FOR = "x-forwarded-for";
+    private static final String NR_SEC_CUSTOM_ATTRIB_NAME = "HTTPSERVER_LOCK-";
+    public static final String HANDLE_METHOD_NAME = "handle";
+    private static final String EMPTY = "";
+    private static final String CONTENT_TYPE = "Content-type";
+    public static final String QUESTION_MARK = "?";
+
+    public static void processHttpRequestHeaders(Headers headers, HttpRequest securityRequest){
+        for (String headerKey : headers.keySet()) {
+            boolean takeNextValue = false;
+            if (headerKey != null) {
+                headerKey = headerKey.toLowerCase();
+            }
+            AgentPolicy agentPolicy = NewRelicSecurity.getAgent().getCurrentPolicy();
+            AgentMetaData agentMetaData = NewRelicSecurity.getAgent().getSecurityMetaData().getMetaData();
+            if (agentPolicy != null && agentPolicy.getProtectionMode().getEnabled()
+                    && agentPolicy.getProtectionMode().getIpBlocking().getEnabled()
+                    && agentPolicy.getProtectionMode().getIpBlocking().getIpDetectViaXFF()
+                    && X_FORWARDED_FOR.equals(headerKey)) {
+                takeNextValue = true;
+            } else if (ServletHelper.CSEC_IAST_FUZZ_REQUEST_ID.equals(headerKey)) {
+                // TODO: May think of removing this intermediate obj and directly create K2 Identifier.
+                NewRelicSecurity.getAgent().getSecurityMetaData()
+                        .setFuzzRequestIdentifier(ServletHelper.parseFuzzRequestIdentifierHeader(headers.getFirst(headerKey)));
+            }
+            String headerFullValue = EMPTY;
+            for (String headerValue : headers.get(headerKey)) {
+                if (headerValue != null && !headerValue.trim().isEmpty()) {
+                    if (takeNextValue) {
+                        agentMetaData.setClientDetectedFromXFF(true);
+                        securityRequest.setClientIP(headerValue);
+                        agentMetaData.getIps()
+                                .add(securityRequest.getClientIP());
+                        securityRequest.setClientPort(EMPTY);
+                        takeNextValue = false;
+                    }
+                    if (headerFullValue.trim().isEmpty()) {
+                        headerFullValue = headerValue;
+                    } else {
+                        headerFullValue = String.join(";", headerFullValue, headerValue);
+                    }
+                }
+            }
+            securityRequest.getHeaders().put(headerKey, headerFullValue);
+        }
+    }
+    public static String getContentType(Headers headers){
+        String data = EMPTY;
+        if (headers.containsKey(CONTENT_TYPE)) {
+            data = headers.getFirst(CONTENT_TYPE);
+        }
+        return data;
+    }
+    public static String getTraceHeader(Map<String, String> headers) {
+        String data = EMPTY;
+        if (headers.containsKey(ServletHelper.CSEC_DISTRIBUTED_TRACING_HEADER) || headers.containsKey(ServletHelper.CSEC_DISTRIBUTED_TRACING_HEADER.toLowerCase())) {
+            data = headers.get(ServletHelper.CSEC_DISTRIBUTED_TRACING_HEADER);
+            if (data == null || data.trim().isEmpty()) {
+                data = headers.get(ServletHelper.CSEC_DISTRIBUTED_TRACING_HEADER.toLowerCase());
+            }
+        }
+        return data;
+    }
+
+    public static boolean acquireServletLockIfPossible() {
+        try {
+            if (NewRelicSecurity.isHookProcessingActive() && !isServletLockAcquired()) {
+                NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(getNrSecCustomAttribName(), true);
+                return true;
+            }
+        } catch (Throwable ignored) {}
+        return false;
+    }
+    public static void releaseServletLock() {
+        try {
+            if(NewRelicSecurity.isHookProcessingActive()) {
+                NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(getNrSecCustomAttribName(), null);
+            }
+        } catch (Throwable ignored){}
+    }
+    private static String getNrSecCustomAttribName() {
+        return NR_SEC_CUSTOM_ATTRIB_NAME + Thread.currentThread().getId();
+    }
+    public static boolean isServletLockAcquired() {
+        try {
+            return NewRelicSecurity.isHookProcessingActive() &&
+                    Boolean.TRUE.equals(NewRelicSecurity.getAgent().getSecurityMetaData().getCustomAttribute(getNrSecCustomAttribName(), Boolean.class));
+        } catch (Throwable ignored) {}
+        return false;
+    }
+
+}
---
+++ instrumentation-security/sun-net-httpserver/src/main/java/com/sun/net/httpserver/Filter_Instrumentation.java
@@ -0,0 +1,109 @@
+package com.sun.net.httpserver;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.instrumentation.helpers.LowSeverityHelper;
+import com.newrelic.api.agent.security.instrumentation.helpers.ServletHelper;
+import com.newrelic.api.agent.security.schema.AgentMetaData;
+import com.newrelic.api.agent.security.schema.HttpRequest;
+import com.newrelic.api.agent.security.schema.SecurityMetaData;
+import com.newrelic.api.agent.security.schema.exceptions.NewRelicSecurityException;
+import com.newrelic.api.agent.security.schema.operation.RXSSOperation;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.security.httpserver.HttpServerHelper;
+
+import java.io.IOException;
+
+@Weave(originalName = "com.sun.net.httpserver.Filter", type = MatchType.Interface)
+public class Filter_Instrumentation {
+    public void doFilter (HttpExchange exchange, Filter.Chain chain) throws IOException {
+        boolean isHttpServerLockAcquired = acquireServletLockIfPossible();
+
+        if (isHttpServerLockAcquired){
+            preprocessSecurityHook(exchange);
+        }
+        try{
+            Weaver.callOriginal();
+        } finally {
+            if (isHttpServerLockAcquired){
+                releaseServletLock();
+            }
+        }
+        if (isHttpServerLockAcquired){
+            postProcessSecurityHook(exchange);
+        }
+    }
+
+    private void preprocessSecurityHook(HttpExchange exchange) {
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive()) {
+                return;
+            }
+            SecurityMetaData securityMetaData = NewRelicSecurity.getAgent().getSecurityMetaData();
+
+            HttpRequest securityRequest = securityMetaData.getRequest();
+            if (securityRequest.isRequestParsed()) {
+                return;
+            }
+
+            AgentMetaData securityAgentMetaData = securityMetaData.getMetaData();
+            securityRequest.setMethod(exchange.getRequestMethod());
+            securityRequest.setClientIP(exchange.getRemoteAddress().getAddress().getHostAddress());
+            securityRequest.setServerPort(exchange.getLocalAddress().getPort());
+
+            if (securityRequest.getClientIP() != null && !securityRequest.getClientIP().trim().isEmpty()) {
+                securityAgentMetaData.getIps().add(securityRequest.getClientIP());
+                securityRequest.setClientPort(String.valueOf(exchange.getRemoteAddress().getPort()));
+            }
+
+            HttpServerHelper.processHttpRequestHeaders(exchange.getRequestHeaders(), securityRequest);
+            securityMetaData.setTracingHeaderValue(HttpServerHelper.getTraceHeader(securityRequest.getHeaders()));
+            securityRequest.setProtocol(exchange.getProtocol());
+            securityRequest.setUrl(String.valueOf(exchange.getRequestURI()));
+
+            String queryString = exchange.getRequestURI().getQuery();
+            if (queryString != null && !queryString.trim().isEmpty()) {
+                securityRequest.setUrl(securityRequest.getUrl() + HttpServerHelper.QUESTION_MARK + queryString);
+            }
+
+            securityRequest.setContentType(HttpServerHelper.getContentType(exchange.getRequestHeaders()));
+
+            securityAgentMetaData.setServiceTrace(Thread.currentThread().getStackTrace());
+            securityRequest.setRequestParsed(true);
+        } catch (Throwable ignored){}
+    }
+    private void postProcessSecurityHook(HttpExchange exchange) {
+        try {
+            if (!NewRelicSecurity.isHookProcessingActive()) {
+                return;
+            }
+            //Add request URI hash to low severity event filter
+            LowSeverityHelper.addRrequestUriToEventFilter(NewRelicSecurity.getAgent().getSecurityMetaData().getRequest());
+
+            RXSSOperation rxssOperation = new RXSSOperation(NewRelicSecurity.getAgent().getSecurityMetaData().getRequest(),
+                    NewRelicSecurity.getAgent().getSecurityMetaData().getResponse(),
+                    this.getClass().getName(), HttpServerHelper.HANDLE_METHOD_NAME);
+            NewRelicSecurity.getAgent().registerOperation(rxssOperation);
+            ServletHelper.tmpFileCleanUp(NewRelicSecurity.getAgent().getSecurityMetaData().getFuzzRequestIdentifier().getTempFiles());
+        } catch (Throwable e) {
+            if(e instanceof NewRelicSecurityException){
+                e.printStackTrace();
+                throw e;
+            }
+        }
+    }
+
+    private boolean acquireServletLockIfPossible() {
+        try {
+            return HttpServerHelper.acquireServletLockIfPossible();
+        } catch (Throwable ignored) {}
+        return false;
+    }
+    private void releaseServletLock() {
+        try {
+            HttpServerHelper.releaseServletLock();
+        } catch (Throwable ignored) {}
+    }
+
+}
