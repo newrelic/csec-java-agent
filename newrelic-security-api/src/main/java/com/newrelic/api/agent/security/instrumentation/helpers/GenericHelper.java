58e27541bf4d085d05e2c93cf97aab47faf9f303
lovesh-ap <lbaya@newrelic.com>
2023-10-30 12:58:11 +0530

    Move release to 17th Dec

--- newrelic-security-api/src/main/java/com/newrelic/api/agent/security/instrumentation/helpers/GenericHelper.java
+++ newrelic-security-api/src/main/java/com/newrelic/api/agent/security/instrumentation/helpers/GenericHelper.java
@@ -4,6 +4,7 @@ import com.newrelic.api.agent.security.NewRelicSecurity;
 
 public class GenericHelper {
     public static final String CSEC_PARENT_ID = "nr-csec-parent-id";
+    public static final String NR_SEC_CUSTOM_SPRING_REDIS_ATTR = "SPRING-DATA-REDIS";
 
     public static boolean skipExistsEvent() {
         if (!(NewRelicSecurity.getAgent().getCurrentPolicy().getVulnerabilityScan().getEnabled() &&
