53a01b7d2b7c693a34d5cc291fc6b13c43b0ffd8
lovesh-ap <lbaya@newrelic.com>
2023-10-30 12:59:53 +0530

    Add RedisOperation and new case type CACHING_DATA_STORE

---
+++ newrelic-security-api/src/main/java/com/newrelic/api/agent/security/schema/operation/RedisOperation.java
@@ -0,0 +1,59 @@
+package com.newrelic.api.agent.security.schema.operation;
+
+import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.VulnerabilityCaseType;
+
+import java.util.List;
+
+public class RedisOperation extends AbstractOperation {
+    public static final String REDIS = "REDIS";
+    private String type;
+
+    private List<Object> arguments;
+
+    private String category;
+
+    private String mode;
+
+    public RedisOperation(String className, String methodName, String type, List<Object> arguments) {
+        super(className, methodName);
+        this.setCaseType(VulnerabilityCaseType.CACHING_DATA_STORE);
+        this.category = REDIS;
+        this.type = type;
+        this.arguments = arguments;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<Object> getArguments() {
+        return arguments;
+    }
+
+    public void setArguments(List<Object> arguments) {
+        this.arguments = arguments;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (type == null || type.trim().isEmpty());
+    }
+
+}
