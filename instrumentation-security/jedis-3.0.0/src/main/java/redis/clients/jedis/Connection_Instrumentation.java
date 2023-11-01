f5a00bc93799515bb433b0cb485ca1c09eabeee6
Monu Lakshkar <mlakshkar@newrelic.com>
2023-11-01 17:41:16 +0530

    code optimisation

--- instrumentation-security/jedis-3.0.0/src/main/java/redis/clients/jedis/Connection_Instrumentation.java
+++ instrumentation-security/jedis-3.0.0/src/main/java/redis/clients/jedis/Connection_Instrumentation.java
@@ -22,19 +22,19 @@ public abstract class Connection_Instrumentation {
         boolean isLockAcquired = JedisHelper.acquireLockIfPossible(cmd.hashCode());
         AbstractOperation operation = null;
         if(isLockAcquired && args != null && args.length > 0) { // args.length>0 will ensure the event generation for the commands with data
-                List<Object> argList = new ArrayList<>();
-                for (int i=0; i < args.length; i++) {
-                    Object dataByBytes = NewRelicSecurity.getAgent()
-                            .getSecurityMetaData()
-                            .getCustomAttribute(GenericHelper.NR_SEC_CUSTOM_SPRING_REDIS_ATTR + args[i].hashCode(), Object.class);
-                    if(dataByBytes!=null){
-                        argList.add(dataByBytes);
-                    } else {
-                        argList.add(new String(args[i]));
-                    }
+            List<Object> argList = new ArrayList<>();
+            for (byte[] arg : args) {
+                Object dataByBytes = NewRelicSecurity.getAgent()
+                        .getSecurityMetaData()
+                        .getCustomAttribute(GenericHelper.NR_SEC_CUSTOM_SPRING_REDIS_ATTR + arg.hashCode(), Object.class);
+                if (dataByBytes != null) {
+                    argList.add(dataByBytes);
+                } else {
+                    argList.add(new String(arg));
                 }
-                operation = JedisHelper.preprocessSecurityHook(new String(cmd.getRaw()), argList, this.getClass().getName(), "sendCommand");
             }
+            operation = JedisHelper.preprocessSecurityHook(new String(cmd.getRaw()), argList, this.getClass().getName(), "sendCommand");
+        }
         try {
             Weaver.callOriginal();
         } finally {
