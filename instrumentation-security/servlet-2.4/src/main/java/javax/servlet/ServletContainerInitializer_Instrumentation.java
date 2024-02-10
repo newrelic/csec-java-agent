04648519dbe9b07882c45f356a7d42c61fc5549f
idawda <idawda@newrelic.com>
2024-02-10 07:55:31 +0530

    Enabled API endpoint support for servlet JSP

--- instrumentation-security/servlet-2.4/src/main/java/javax/servlet/ServletContainerInitializer_Instrumentation.java
+++ instrumentation-security/servlet-2.4/src/main/java/javax/servlet/ServletContainerInitializer_Instrumentation.java
@@ -1,11 +1,13 @@
 package javax.servlet;
 
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.newrelic.agent.security.instrumentation.servlet24.HttpServletHelper;
 
 import java.util.Set;
 
-//@Weave(type = MatchType.Interface, originalName = "javax.servlet.ServletContainerInitializer")
+@Weave(type = MatchType.Interface, originalName = "javax.servlet.ServletContainerInitializer")
 public class ServletContainerInitializer_Instrumentation {
     public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
         try {
