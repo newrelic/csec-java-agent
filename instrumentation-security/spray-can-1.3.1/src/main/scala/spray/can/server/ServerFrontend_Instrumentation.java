6a957b858f4f8bc6681c450085d64d00ec54c1d3
lovesh-ap <lbaya@newrelic.com>
2024-02-27 10:57:17 +0530

    Remove dispatcher true from wevaved methods

--- instrumentation-security/spray-can-1.3.1/src/main/scala/spray/can/server/ServerFrontend_Instrumentation.java
+++ instrumentation-security/spray-can-1.3.1/src/main/scala/spray/can/server/ServerFrontend_Instrumentation.java
@@ -16,7 +16,7 @@ import spray.http.HttpRequest;
 
 @Weave(originalName = "spray.can.server.ServerFrontend$$anon$2$$anon$1")
 public class ServerFrontend_Instrumentation {
-    @Trace(dispatcher = true)
+
     public void spray$can$server$ServerFrontend$$anon$$anon$$openNewRequest(final HttpRequest request,
             final boolean closeAfterResponseCompletion, final RequestState state) {
         boolean isLockAcquired = GenericHelper.acquireLockIfPossible(SprayHttpUtils.getNrSecCustomAttribName());
