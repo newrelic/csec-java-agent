c370c9c504325266d4a35ca2144129633e5ae0d3
Ishika Dawda <112611034+IshikaDawda@users.noreply.github.com>
2024-02-28 10:33:16 +0530

    API endpoint support for Mule server (#196)

---
+++ instrumentation-security/mule-3.6/src/main/java/org/mule/processor/InvokerMessageProcessor_Instrumentation.java
@@ -0,0 +1,20 @@
+package org.mule.processor;
+
+import com.newrelic.agent.security.instrumentation.mule36.MuleHelper;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import org.mule.api.lifecycle.InitialisationException;
+
+@Weave(originalName = "org.mule.processor.InvokerMessageProcessor", type = MatchType.ExactClass)
+public class InvokerMessageProcessor_Instrumentation {
+    protected Object object = Weaver.callOriginal();
+
+    public void initialise() throws InitialisationException {
+        try {
+            Weaver.callOriginal();
+        } finally {
+            MuleHelper.getHandlerMap().put(hashCode(), object.getClass().getName());
+        }
+    }
+}
