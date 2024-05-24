11bce32cbebc3fd3506f4ebe52b291d7ac0c9be0
idawda <idawda@newrelic.com>
2024-05-24 15:51:21 +0530

    API Endpoint Phase 2: Add Endpoint route in Jersey Framework

---
+++ instrumentation-security/jersey/src/main/java/org/glassfish/jersey/uri/PatternWithGroups_Instrumentation.java
@@ -0,0 +1,23 @@
+package org.glassfish.jersey.uri;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+import java.util.regex.MatchResult;
+
+@Weave(originalName = "org.glassfish.jersey.uri.PatternWithGroups", type = MatchType.BaseClass)
+public class PatternWithGroups_Instrumentation {
+    public final MatchResult match(final CharSequence cs) {
+        MatchResult result = Weaver.callOriginal();
+        try {
+            if (NewRelicSecurity.isHookProcessingActive() && result != null && this instanceof PathPattern_Instrumentation && ((PathPattern_Instrumentation) this).getTemplate() != null){
+                String segment = ((PathPattern_Instrumentation) this).getTemplate().getTemplate();
+                NewRelicSecurity.getAgent().getSecurityMetaData().getMetaData().setEndpointRoute(segment);
+            }
+        } catch (Exception e) {
+        }
+        return result;
+    }
+}
