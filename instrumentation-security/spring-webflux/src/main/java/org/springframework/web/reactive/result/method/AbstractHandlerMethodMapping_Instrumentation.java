321b03ea5e45de2daf190d2c843e9f84444dbb5d
Ishika Dawda <112611034+IshikaDawda@users.noreply.github.com>
2024-06-14 11:44:28 +0530

    [NR-273605] API Endpoint Phase 2 : Detect route of an endpoint for Spring-WebFlux Framework (#258)
    * API Endpoint Phase 2: Add Endpoint route in Spring-WebFlux Framework

--- instrumentation-security/spring-webflux/src/main/java/org/springframework/web/reactive/result/method/AbstractHandlerMethodMapping_Instrumentation.java
+++ instrumentation-security/spring-webflux/src/main/java/org/springframework/web/reactive/result/method/AbstractHandlerMethodMapping_Instrumentation.java
@@ -3,10 +3,11 @@ package org.springframework.web.reactive.result.method;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
-
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.server.ServerWebExchange;
 import java.lang.reflect.Method;
 
-@Weave(type = MatchType.ExactClass, originalName = "org.springframework.web.reactive.result.method.AbstractHandlerMethodMapping")
+@Weave(type = MatchType.BaseClass, originalName = "org.springframework.web.reactive.result.method.AbstractHandlerMethodMapping")
 public abstract class AbstractHandlerMethodMapping_Instrumentation<T> {
 
     protected void registerHandlerMethod(Object handler, Method method, T mapping) {
@@ -15,5 +16,10 @@ public abstract class AbstractHandlerMethodMapping_Instrumentation<T> {
         } finally {
             SpringHelper.gatherURLMappings(mapping, method);
         }
+    }
+
+    protected void handleMatch(T mapping, HandlerMethod handlerMethod, ServerWebExchange exchange) {
+        Weaver.callOriginal();
+        SpringHelper.getRequestRoute(mapping);
     }
 }
