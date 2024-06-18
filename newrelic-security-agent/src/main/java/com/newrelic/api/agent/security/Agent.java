4ff2144f7f67a7663cbc53ca3a099a00d75e3702
Ishika Dawda <112611034+IshikaDawda@users.noreply.github.com>
2024-06-18 09:52:55 +0530

    Instrumentation Support for Vert.x-core  (#245)
    * Instrument vertx core webclient request interception for version 3.8.0
    
    * Support for vertx core webclient version 3.3.0
    
    * Support for vertx core webclient version 3.4.0
    
    * Support for vertx core webclient version 4.0.0
    
    * Fix for dropping events in vertx support
    
    * [NR-254167] Unit tests for Vertx-Core Instrumentation Support (#273)
    ---------
    
    Co-authored-by: lovesh-ap <lbaya@newrelic.com>

--- newrelic-security-agent/src/main/java/com/newrelic/api/agent/security/Agent.java
+++ newrelic-security-agent/src/main/java/com/newrelic/api/agent/security/Agent.java
@@ -355,7 +355,11 @@ public class Agent implements SecurityAgent {
     private static boolean checkIfNRGeneratedEvent(AbstractOperation operation) {
         boolean isNettyReactor = false, isNRGeneratedEvent = false;
         for (int i = 1, j = 0; i < operation.getStackTrace().length; i++) {
-            if(StringUtils.equalsAny(operation.getStackTrace()[i].getClassName(), "com.nr.instrumentation.TokenLinkingSubscriber", "com.nr.instrumentation.reactor.netty.TokenLinkingSubscriber")){
+            if(StringUtils.equalsAny(operation.getStackTrace()[i].getClassName(),
+                    "com.nr.instrumentation.TokenLinkingSubscriber",
+                    "com.nr.instrumentation.reactor.netty.TokenLinkingSubscriber",
+                    "com.nr.vertx.instrumentation.VertxUtil$1",
+                    "com.nr.vertx.instrumentation.HttpClientRequestPromiseWrapper")){
                 isNettyReactor = true;
                 continue;
             }
