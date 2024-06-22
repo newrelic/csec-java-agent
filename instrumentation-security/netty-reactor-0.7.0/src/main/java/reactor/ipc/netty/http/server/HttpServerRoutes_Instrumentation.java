f12553081ae05650e93466337a767e65463fc64e
Ishika Dawda <112611034+IshikaDawda@users.noreply.github.com>
2024-06-22 10:46:36 +0530

    [NR-267158] API Endpoint Support For Netty Reactor (#254)
    * Update comparing methods in ApplicationURLMapping
    
    * API Endpoint Support for netty version 0.8.0.RELEASE & above
    
    * API Endpoint Support for netty reactor version [0.6.0.RELEASE,0.8.0.RELEASE)

---
+++ instrumentation-security/netty-reactor-0.7.0/src/main/java/reactor/ipc/netty/http/server/HttpServerRoutes_Instrumentation.java
@@ -0,0 +1,39 @@
+package reactor.ipc.netty.http.server;
+
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.instrumentation.helpers.GenericHelper;
+import com.newrelic.api.agent.security.instrumentation.helpers.URLMappingsHelper;
+import com.newrelic.api.agent.security.schema.ApplicationURLMapping;
+import com.newrelic.api.agent.security.schema.StringUtils;
+import com.newrelic.api.agent.security.utils.logging.LogLevel;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import org.reactivestreams.Publisher;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
+@Weave(originalName = "reactor.ipc.netty.http.server.HttpServerRoutes", type = MatchType.Interface)
+public class HttpServerRoutes_Instrumentation {
+
+    public HttpServerRoutes_Instrumentation route( Predicate<? super HttpServerRequest> condition, BiFunction<? super HttpServerRequest, ? super HttpServerResponse, ? extends Publisher<Void>> handler) {
+        HttpServerRoutes_Instrumentation result = Weaver.callOriginal();
+        addURLMapping(condition, handler.getClass().getName());
+        return result;
+    }
+    private void addURLMapping(Predicate<? super HttpServerRequest> condition, String className){
+        try {
+            className = StringUtils.substringBefore(className, URLMappingsHelper.SEPARATOR);
+            ApplicationURLMapping mapping;
+            if (condition instanceof HttpPredicate){
+                HttpPredicate endpoint = (HttpPredicate) condition;
+                mapping = new ApplicationURLMapping(endpoint.method.name(), endpoint.uri, className);
+            } else {
+                mapping = new ApplicationURLMapping(URLMappingsHelper.WILDCARD, URLMappingsHelper.WILDCARD, className);
+            }
+            URLMappingsHelper.addApplicationURLMapping(mapping);
+        } catch (Exception e){
+            NewRelicSecurity.getAgent().log(LogLevel.WARNING, String.format(GenericHelper.ERROR_WHILE_GETTING_APP_ENDPOINTS, "NETTY-REACTOR-0.7.0", e.getMessage()), e, this.getClass().getName());
+        }
+    }
+}
