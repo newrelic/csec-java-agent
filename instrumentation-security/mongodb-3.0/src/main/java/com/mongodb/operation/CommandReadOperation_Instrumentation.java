006b266a413d3ca649dae32166e4ecfcb025e6f7
lovesh-ap <lbaya@newrelic.com>
2024-07-16 18:09:19 +0530

    MongoDB driver version 3.0 to latest support for reporting db connection

--- instrumentation-security/mongodb-3.0/src/main/java/com/mongodb/operation/CommandReadOperation_Instrumentation.java
+++ instrumentation-security/mongodb-3.0/src/main/java/com/mongodb/operation/CommandReadOperation_Instrumentation.java
@@ -1,14 +1,21 @@
 package com.mongodb.operation;
 
+import com.mongodb.ServerAddress;
 import com.mongodb.async.SingleResultCallback;
 import com.mongodb.binding.AsyncReadBinding;
 import com.mongodb.binding.ReadBinding;
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.instrumentation.helpers.GenericHelper;
 import com.newrelic.api.agent.security.schema.AbstractOperation;
+import com.newrelic.api.agent.security.schema.ExternalConnectionType;
+import com.newrelic.api.agent.security.utils.logging.LogLevel;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.newrelic.agent.security.instrumentation.mongo.MongoUtil;
 import org.bson.BsonDocument;
+
+import java.net.*;
 
 @Weave(type = MatchType.ExactClass, originalName = "com.mongodb.operation.CommandReadOperation")
 public class CommandReadOperation_Instrumentation<T> {
@@ -20,6 +27,7 @@ public class CommandReadOperation_Instrumentation<T> {
         boolean isLockAcquired = MongoUtil.acquireLockIfPossible(this.hashCode());
         if (isLockAcquired) {
             noSQLOperation = MongoUtil.recordMongoOperation(command, MongoUtil.OP_READ, this.getClass().getName(), MongoUtil.METHOD_EXECUTE);
+            recordExternalConnection(binding);
         }
         T returnVal = null;
         try {
@@ -31,6 +39,17 @@ public class CommandReadOperation_Instrumentation<T> {
         }
         MongoUtil.registerExitOperation(isLockAcquired, noSQLOperation);
         return returnVal;
+    }
+
+    private static void recordExternalConnection(ReadBinding binding) {
+        ServerAddress serverAddress = binding.getReadConnectionSource().getServerDescription().getAddress();
+        String ipAddress = null;
+        try {
+            ipAddress = InetAddress.getByName(serverAddress.getHost()).getHostAddress();
+        } catch (UnknownHostException ignored) {
+        }
+        NewRelicSecurity.getAgent().recordExternalConnection(serverAddress.getHost(), serverAddress.getPort(),
+                serverAddress.toString(), ipAddress, ExternalConnectionType.DATABASE_CONNECTION.name(), MongoUtil.MONGODB_3_0);
     }
 
     public void executeAsync(final AsyncReadBinding binding, final SingleResultCallback<T> callback) {
---
+++ instrumentation-security/mongodb-3.7/src/main/java/com/mongodb/ServerAddress_Instrumentation.java
@@ -0,0 +1,24 @@
+package com.mongodb;
+
+import com.newrelic.agent.security.instrumentation.mongo.MongoUtil;
+import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.ExternalConnectionType;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Weave(type = MatchType.ExactClass, originalName = "com.mongodb.ServerAddress")
+public class ServerAddress_Instrumentation {
+
+    public ServerAddress_Instrumentation(final String host, final int port) {
+        String ipAddress = null;
+        try {
+            ipAddress = InetAddress.getByName(host).getHostAddress();
+        } catch (UnknownHostException ignored) {
+        }
+        NewRelicSecurity.getAgent().recordExternalConnection(host, port,
+                 null, ipAddress, ExternalConnectionType.DATABASE_CONNECTION.name(), MongoUtil.MONGODB_3_7);
+    }
+}
