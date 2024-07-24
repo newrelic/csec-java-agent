43f72a3e3540dd30757c4e5034741695cf741804
idawda <idawda@newrelic.com>
2024-07-24 19:39:55 +0530

    ExternalConnection reporting for R2DBC driver

--- instrumentation-security/r2dbc-h2/src/main/java/io/r2dbc/h2/client/SessionClient_Instrumentation.java
+++ instrumentation-security/r2dbc-h2/src/main/java/io/r2dbc/h2/client/SessionClient_Instrumentation.java
@@ -1,6 +1,7 @@
 package io.r2dbc.h2.client;
 
 import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.ExternalConnectionType;
 import com.newrelic.api.agent.security.schema.R2DBCVendor;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
@@ -8,9 +9,11 @@ import org.h2.engine.ConnectionInfo;
 
 @Weave(type = MatchType.ExactClass, originalName = "io.r2dbc.h2.client.SessionClient")
 public class SessionClient_Instrumentation {
+
     public SessionClient_Instrumentation(ConnectionInfo connectionInfo, boolean shutdownDatabaseOnClose) {
         if (NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
             NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(R2DBCVendor.META_CONST_R2DBC_VENDOR, R2DBCVendor.H2);
         }
+        NewRelicSecurity.getAgent().recordExternalConnection(null, -1, connectionInfo.getURL(), null, ExternalConnectionType.DATABASE_CONNECTION.name(), "R2DBC-H2");
     }
 }
--- instrumentation-security/r2dbc-mariadb/src/main/java/org/mariadb/r2dbc/MariadbConnectionFactory_Instrumentation.java
+++ instrumentation-security/r2dbc-mariadb/src/main/java/org/mariadb/r2dbc/MariadbConnectionFactory_Instrumentation.java
@@ -1,6 +1,7 @@
 package org.mariadb.r2dbc;
 
 import com.newrelic.api.agent.security.NewRelicSecurity;
+import com.newrelic.api.agent.security.schema.ExternalConnectionType;
 import com.newrelic.api.agent.security.schema.R2DBCVendor;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
@@ -8,12 +9,19 @@ import com.newrelic.api.agent.weaver.Weaver;
 import org.mariadb.r2dbc.api.MariadbConnection;
 import reactor.core.publisher.Mono;
 
-@Weave(type = MatchType.Interface, originalName = "org.mariadb.r2dbc.MariadbConnectionFactory")
+import java.net.SocketAddress;
+
+@Weave(type = MatchType.ExactClass, originalName = "org.mariadb.r2dbc.MariadbConnectionFactory")
 public class MariadbConnectionFactory_Instrumentation {
+
+    private final SocketAddress endpoint = Weaver.callOriginal();
+    private final MariadbConnectionConfiguration configuration = Weaver.callOriginal();
+
     public Mono<MariadbConnection> create() {
         if (NewRelicSecurity.isHookProcessingActive() && !NewRelicSecurity.getAgent().getSecurityMetaData().getRequest().isEmpty()) {
             NewRelicSecurity.getAgent().getSecurityMetaData().addCustomAttribute(R2DBCVendor.META_CONST_R2DBC_VENDOR, R2DBCVendor.MARIA_DB);
         }
+        NewRelicSecurity.getAgent().recordExternalConnection(configuration.getHost(), configuration.getPort(), endpoint.toString(), null, ExternalConnectionType.DATABASE_CONNECTION.name(), "R2DBC-MARIADB");
         return Weaver.callOriginal();
     }
 }
