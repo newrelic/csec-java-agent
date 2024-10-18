3a24ea8ea58809f9f350d7852145e008c5e8840d
idawda <idawda@newrelic.com>
2024-10-18 16:54:34 +0530

    NR-325523: Incoming request interception support in Http4s Blaze Server

---
+++ instrumentation-security/http4s-blaze-server-2.12_0.23/src/main/scala/com/newrelic/agent/security/http4s/blaze/server/RequestProcessor.scala
@@ -0,0 +1,122 @@
+package com.newrelic.agent.security.http4s.blaze.server
+
+import cats.data.Kleisli
+import cats.effect.Sync
+import cats.implicits._
+import com.comcast.ip4s.Port
+import com.newrelic.api.agent.security.NewRelicSecurity
+import com.newrelic.api.agent.security.instrumentation.helpers.{GenericHelper, ICsecApiConstants, ServletHelper}
+import com.newrelic.api.agent.security.schema._
+import com.newrelic.api.agent.security.schema.exceptions.NewRelicSecurityException
+import com.newrelic.api.agent.security.schema.operation.RXSSOperation
+import com.newrelic.api.agent.security.schema.policy.AgentPolicy
+import com.newrelic.api.agent.security.utils.logging.LogLevel
+import org.http4s.{Headers, Request, Response}
+
+import java.util
+
+
+object RequestProcessor {
+
+  private val METHOD_WITH_HTTP_APP = "withHttpApp"
+  private val HTTP_4S_EMBER_SERVER_2_12_0_23 = "HTTP4S-BLAZE-SERVER-2.12_0.23"
+  private val X_FORWARDED_FOR = "x-forwarded-for"
+
+  def genHttpApp[F[_] : Sync](httpApp: Kleisli[F, Request[F], Response[F]]): Kleisli[F, Request[F], Response[F]] = {
+    Kleisli { req: Request[F] => nrRequestResponse(req, httpApp) }
+  }
+
+  private def nrRequestResponse[F[_] : Sync](request: Request[F], httpApp: Kleisli[F, Request[F], Response[F]]): F[Response[F]] = {
+    val result = construct((): Unit)
+      .redeemWith(_ => httpApp(request),
+        _ => for {
+          _ <- preprocessHttpRequest(request)
+          resp <- httpApp(request)
+        } yield resp
+      )
+    result
+  }
+
+  private def preprocessHttpRequest[F[_]: Sync](request: Request[F]): F[Unit] = construct {
+    val isLockAcquired = BlazeUtils.acquireLockIfPossible()
+    try {
+      if (NewRelicSecurity.isHookProcessingActive && isLockAcquired && !NewRelicSecurity.getAgent.getSecurityMetaData.getRequest.isRequestParsed){
+
+        val securityMetaData: SecurityMetaData = NewRelicSecurity.getAgent.getSecurityMetaData
+        val securityRequest: HttpRequest = securityMetaData.getRequest
+        val securityAgentMetaData: AgentMetaData = securityMetaData.getMetaData
+
+        securityRequest.setMethod(request.method.name)
+        securityRequest.setServerPort((request.serverPort).get.asInstanceOf[Port].value)
+        securityRequest.setClientIP(request.remoteAddr.get.toString)
+        securityRequest.setProtocol(BlazeUtils.getProtocol(request.isSecure.get))
+        securityRequest.setUrl(request.uri.toString)
+
+        if (securityRequest.getClientIP != null && securityRequest.getClientIP.trim.nonEmpty) {
+          securityAgentMetaData.getIps.add(securityRequest.getClientIP)
+          securityRequest.setClientPort(String.valueOf(request.remotePort.get))
+        }
+
+        processRequestHeaders(request.headers, securityRequest)
+        securityMetaData.setTracingHeaderValue(BlazeUtils.getTraceHeader(securityRequest.getHeaders))
+        securityRequest.setContentType(BlazeUtils.getContentType(securityRequest.getHeaders))
+
+        // TODO extract request body & user class detection
+
+        val trace: Array[StackTraceElement] = Thread.currentThread.getStackTrace
+        securityMetaData.getMetaData.setServiceTrace(util.Arrays.copyOfRange(trace, 1, trace.length))
+        securityRequest.setRequestParsed(true)
+      }
+
+    } catch {
+      case e: Throwable => NewRelicSecurity.getAgent.log(LogLevel.WARNING, String.format(GenericHelper.ERROR_GENERATING_HTTP_REQUEST, HTTP_4S_EMBER_SERVER_2_12_0_23, e.getMessage), e, this.getClass.getName)
+    } finally {
+      if (isLockAcquired) {
+        BlazeUtils.releaseLock()
+      }
+    }
+  }
+
+  private def processRequestHeaders(headers: Headers, securityRequest: HttpRequest): Unit = {
+    headers.foreach(header => {
+      var takeNextValue = false
+      var headerKey: String = StringUtils.EMPTY
+      if (header.name != null && header.name.nonEmpty) {
+        headerKey = header.name.toString
+      }
+      val headerValue: String = header.value
+
+      val agentPolicy: AgentPolicy = NewRelicSecurity.getAgent.getCurrentPolicy
+      val agentMetaData: AgentMetaData = NewRelicSecurity.getAgent.getSecurityMetaData.getMetaData
+      if (agentPolicy != null
+        && agentPolicy.getProtectionMode.getEnabled()
+        && agentPolicy.getProtectionMode.getIpBlocking.getEnabled()
+        && agentPolicy.getProtectionMode.getIpBlocking.getIpDetectViaXFF()
+        && X_FORWARDED_FOR.equals(headerKey)) {
+        takeNextValue = true
+      } else if (ServletHelper.CSEC_IAST_FUZZ_REQUEST_ID == headerKey) {
+        // TODO: May think of removing this intermediate obj and directly create K2 Identifier.
+        NewRelicSecurity.getAgent.getSecurityMetaData.setFuzzRequestIdentifier(ServletHelper.parseFuzzRequestIdentifierHeader(headerValue))
+      }
+      if (GenericHelper.CSEC_PARENT_ID == headerKey) {
+        NewRelicSecurity.getAgent.getSecurityMetaData.addCustomAttribute(GenericHelper.CSEC_PARENT_ID, headerValue)
+      }
+      else if (ICsecApiConstants.NR_CSEC_JAVA_HEAD_REQUEST == headerKey) {
+        NewRelicSecurity.getAgent.getSecurityMetaData.addCustomAttribute(ICsecApiConstants.NR_CSEC_JAVA_HEAD_REQUEST, true)
+      }
+
+      if (headerValue != null && headerValue.trim.nonEmpty) {
+        if (takeNextValue) {
+          agentMetaData.setClientDetectedFromXFF(true)
+          securityRequest.setClientIP(headerValue)
+          agentMetaData.getIps.add(securityRequest.getClientIP)
+          securityRequest.setClientPort(StringUtils.EMPTY)
+          takeNextValue = false
+        }
+      }
+      securityRequest.getHeaders.put(headerKey.toLowerCase, headerValue)
+    })
+  }
+
+  private def construct[F[_]: Sync, T](t: => T): F[T] = Sync[F].delay(t)
+}
