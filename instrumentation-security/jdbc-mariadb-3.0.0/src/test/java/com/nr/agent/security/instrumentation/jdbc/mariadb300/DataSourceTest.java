7336d576306393690247d68f5a9bb9b56fcd28eb
idawda <idawda@newrelic.com>
2025-01-16 19:08:42 +0530

    Add instrumentation support and Unit tests for Mariadb 3.0.0 till latest

---
+++ instrumentation-security/jdbc-mariadb-3.0.0/src/test/java/com/nr/agent/security/instrumentation/jdbc/mariadb300/DataSourceTest.java
@@ -0,0 +1,119 @@
+package com.nr.agent.security.instrumentation.jdbc.mariadb300;
+
+import ch.vorburger.mariadb4j.DB;
+import ch.vorburger.mariadb4j.DBConfigurationBuilder;
+import com.newrelic.agent.security.introspec.InstrumentationTestConfig;
+import com.newrelic.agent.security.introspec.SecurityInstrumentationTestRunner;
+import com.newrelic.agent.security.introspec.SecurityIntrospector;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.security.schema.JDBCVendor;
+import com.newrelic.api.agent.security.schema.StringUtils;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SecurityInstrumentationTestRunner.class)
+@InstrumentationTestConfig(includePrefixes = {"org.mariadb.jdbc"})
+public class DataSourceTest {
+    private static DB mariaDb;
+    private static String connectionString;
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+
+    @BeforeClass
+    public static void setUpDb() throws Exception {
+        DBConfigurationBuilder builder = DBConfigurationBuilder.newBuilder()
+                .setPort(0); // This will automatically find a free port
+
+        String dbName = "MariaDB" + System.currentTimeMillis();
+        mariaDb = DB.newEmbeddedDB(builder.build());
+        connectionString = StringUtils.replace(builder.getURL(dbName), "mysql", "mariadb");
+        mariaDb.start();
+
+        mariaDb.createDB(dbName);
+        mariaDb.source("maria-db-test.sql", null, null, dbName);
+    }
+    @AfterClass
+    public static void tearDownDb() throws Exception {
+        mariaDb.stop();
+    }
+
+    @Test
+    public void testConnection() throws SQLException {
+        getConnection( new MariaDbDataSource());
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MARIA_DB, vendor);
+    }
+
+    @Test
+    public void testConnection1() throws SQLException {
+        getConnection1(new MariaDbDataSource());
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MARIA_DB, vendor);
+    }
+
+    @Test
+    public void testConnection2() throws SQLException {
+        getConnection( new MariaDbDataSource(connectionString));
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MARIA_DB, vendor);
+    }
+
+    @Test
+    public void testConnection3() throws SQLException {
+        getConnection1(new MariaDbDataSource(connectionString));
+
+        SecurityIntrospector introspector = SecurityInstrumentationTestRunner.getIntrospector();
+        String vendor = introspector.getJDBCVendor();
+        Assert.assertEquals("Incorrect DB vendor", JDBCVendor.MARIA_DB, vendor);
+    }
+
+    @Trace(dispatcher = true)
+    private void getConnection(MariaDbDataSource dataSource) throws SQLException {
+        Connection dbConnection = null;
+
+        dataSource.setUrl(connectionString);
+        dataSource.setUser(DB_USER);
+        dataSource.setPassword(DB_PASSWORD);
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            dbConnection = dataSource.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (dbConnection!=null) {
+                dbConnection.close();
+            }
+        }
+    }
+
+    @Trace(dispatcher = true)
+    private void getConnection1(MariaDbDataSource dataSource) throws SQLException {
+        Connection dbConnection = null;
+        dataSource.setUrl(connectionString);
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            dbConnection = dataSource.getConnection(DB_USER, DB_PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (dbConnection!=null) {
+                dbConnection.close();
+            }
+        }
+    }
+}
